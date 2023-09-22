469c6b7a5b1b09223369aaf9576ed7be7eb9afe0
mashulyag <ya.merri2005@yandex.ru>
2023-09-22 17:51:38 +0300

    MGLaba1

--- tasks/linux/tasks/5/2.bash
+++ tasks/linux/tasks/5/2.bash
@@ -1,2 +1,3 @@
-# Создайте папку ./Turing/Markov/ZZZ при условии, что папок Turing и Markov не существует
+ Создайте папку ./Turing/Markov/ZZZ при условии, что папок Turing и Markov не существует
+ mkdir -p ./Turing/Markov/ZZZ
 
