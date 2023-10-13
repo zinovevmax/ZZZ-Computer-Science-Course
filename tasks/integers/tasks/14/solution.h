3a8860f85eb7f15efece0d2d5b2d43b76b108a8e
winterfulllll <masterfm65@gmail.com>
2023-10-14 01:21:57 +0300

    l6

--- tasks/integers/tasks/14/solution.h
+++ tasks/integers/tasks/14/solution.h
@@ -3,7 +3,27 @@
 #include <stdint.h>
 #include <stdio.h>
 
+const int64_t TEN = 10;
+
 int Task() {
-    // write your solution here
+    int64_t num = 0;
+    bool flag = false;
+    scanf("%lld", &num);
+    if (num < TEN) {
+        printf("No");
+    } else {
+        while (num >= TEN) {
+            if (num % TEN == (num / TEN) % TEN) {
+                flag = true;
+                break;
+            }
+            num /= TEN;
+        }
+        if (flag) {
+            printf("Yes");
+        } else {
+            printf("No");
+        }
+    }
     return 0;
 }
