2d7635534f0f6bd798235c050c7495baa8a23e95
Ivan <surov.804@mail.ru>
2023-10-14 19:39:11 +0300

    new_geom

--- tasks/geometry/tasks/12/solution.h
+++ tasks/geometry/tasks/12/solution.h
@@ -10,10 +10,10 @@ const int I_FIRST = -22;
 const int J_FIRST = 29;
 const int L_FIRST = 4;
 
-using Point = struct {
+typedef struct {
     int x;
     int y;
-};
+} Point;
 
 int Min(int num_1, int num_2) {
     if (num_1 < num_2) {
@@ -30,16 +30,17 @@ int Max(int num_1, int num_2) {
 }
 
 int Mod(int num, int base) {
-    if (num > 0) {
-        while (num > base) {
-            num -= base;
+    int remains = num;
+    if (remains > 0) {
+        while (remains > base) {
+            remains -= base;
         }
     } else {
-        while (num < 0) {
-            num += base;
+        while (remains < 0) {
+            remains += base;
         }
     }
-    return num;
+    return remains;
 }
 
 int Sign(int num) {
@@ -59,21 +60,18 @@ int Abs(int num) {
 }
 
 int Pow(int num, int degree) {
+    int result = num;
     while (degree != 1) {
-        num *= num;
+        result *= result;
         degree -= 1;
     }
-    return num;
+    return result;
 }
 
 int CheckZone(Point p) {
     // Write a code here that checks whether a point belongs to a given area
-    if (((Pow(p.x - TEN, 2) + Pow(p.y - TEN, 2)) <= Pow(TEN, 2)) &&
-        ((Pow(p.x - TWENTY, 2) + Pow(p.y - TWENTY, 2)) <= Pow(TEN, 2))) {
-        return 1;
-    } else {
-        return 0;
-    }
+    return (((Pow(p.x - TEN, 2) + Pow(p.y - TEN, 2)) <= Pow(TEN, 2)) &&
+            ((Pow(p.x - TWENTY, 2) + Pow(p.y - TWENTY, 2)) <= Pow(TEN, 2)));
 }
 
 int Task() {
@@ -92,6 +90,9 @@ int Task() {
         printf("k = %d, x = %d, y = %d, l = %d belong = %d \n", k, p.x, p.y, l, CheckZone(p));
         if (CheckZone(p)) {
             break;
+        }
+        if (k == ITERATIONS) {
+            printf("The point did not fall into the specified area for %d iterations", ITERATIONS);
         }
     }
     return 0;
