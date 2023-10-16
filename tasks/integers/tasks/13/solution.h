4cdf2a65164a123b950bba438b7216c799e58026
Matvey26 <matvey.ivchenko@yandex.ru>
2023-10-16 18:37:54 +0300

    1.2 solution

--- tasks/integers/tasks/13/solution.h
+++ tasks/integers/tasks/13/solution.h
@@ -2,50 +2,65 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-// Максимальная длина 8-байтового числа (нужно для записи в строку)
-const size_t MAX_NUMBER_LEN = 20;
+// Возводит двойку в нужную степень
+int64_t Pow2(size_t power) {
+    int64_t result = 1;
+    while (power > 0) {
+        result *= 2;
+        --power;
+    }
+    return result;
+}
 
-int Task() {
-    // Получаем число в виде строки
-    char str_number[MAX_NUMBER_LEN];
-    printf("Enter the number with leading zeros: ");
-    scanf("%s", str_number);
-
-    // Подсчитываем количество ведущих нулей, а также смотрим на знак числа
-    int16_t count_of_zeros = 0;
-    int16_t is_negative = 0;
-
-    for (size_t i = 0; i < MAX_NUMBER_LEN; ++i) {
-        if (str_number[i] == '-') {
-            is_negative = 1;
-        } else if (str_number[i] != '0') {
-            break;
-        } else {
-            ++count_of_zeros;
+// Считывает символы, пока не встретит перевод строки. Подсчитывает среди них нули
+size_t CountZeros() {
+    size_t zero_count = 0;
+    char zero = ' ';
+    while (zero != '\n') {
+        scanf("%c", &zero);
+        if (zero == '0') {
+            ++zero_count;
         }
     }
+    return zero_count; 
+}
 
-    // Далее будем работать с введённым числом как с числом, а не строкой
-    int64_t number = atol(str_number);
-    number *= (is_negative) ? -1 : 1;
-
-    // Теперь нужно узнать, сколько знаков будет в двоичном представлении
-    int64_t number_copy = number;
-    size_t bin_len = 0;
-    while (number_copy > 0) {
-        ++bin_len;
-        number_copy /= 2;
+// Подсчитывает длину числа в довичном представлении
+size_t CountBinaryLen(int64_t n) {
+    if (n == 0) {
+        return 1l;
     }
-    
-    // Переводим число в двоичную систему счисления с добавлением нулей в середину
-    size_t half_bin_len = (bin_len + 1) >> 1;
-    int64_t number_left = (number >> half_bin_len) << half_bin_len;
-    int64_t number_right = number - number_left;
-    int64_t result = (number_left << count_of_zeros) + number_right;
+    size_t bin_len = 0;
+    while (n != 0) {
+        n /= 2l;
+        ++bin_len;
+    }
+    return bin_len;
+}
 
-    printf("%ld", (is_negative) ? -result : result);
+// Решает поставленную задачу
+int64_t Solve(int64_t number, size_t zero_count, size_t bin_len) {
+    int64_t result = 0;
+    size_t half_bin_len = (bin_len + 1) >> 1;
+    
+    // Прибавляем правую часть числа без изменений
+    result += number & (Pow2(half_bin_len + 1) - 1);
+    // Прибавляем левую часть числа, но с учётом вставленных нулей
+    result += (number - result) << (bin_len - half_bin_len);
+    return result;
+}
+
+int Task() {
+    // Считаем количество нулей и считываем число
+    size_t zero_count = CountZeros();
+    int64_t number = 0;
+    scanf("%ld", &number);
+
+    // Считаем длину двоичного представления и решаем задачу
+    size_t bin_len = CountBinaryLen(number);
+    int64_t result = Solve(number, zero_count, bin_len);
+    printf("%ld\n", (number < 0) ? -result : result);
 
     return 0;
 }
