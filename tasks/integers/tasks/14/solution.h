6da0402955fe272a561e6c5f00d2ea6bba656ba5
winterfulllll <masterfm65@gmail.com>
2023-10-17 17:24:15 +0300

    l6_3

--- tasks/integers/tasks/14/solution.h
+++ tasks/integers/tasks/14/solution.h
@@ -4,23 +4,32 @@
 #include <stdio.h>
 
 const int64_t TEN = 10;
-const int64_t INPUT_NUM = 12345678987654321;
+const int64_t INPUT_NUM = -123456789876543211;
+
+int64_t Abs(int64_t x) {
+    return (x >= 0) ? x : -x;
+}
+
+bool HasTwoDigits(int64_t x) {
+    return x < TEN;
+}
 
 int Task() {
     int64_t num = INPUT_NUM;
-    int flag = 0;
+    num = Abs(num);
+    bool IsSatisfyingCondition = false;
     // scanf("%lld", &num);
-    if (num < TEN) {
-        printf("No");
+    if (HasTwoDigits(num)) {
+        printf("No\n");
     } else {
         while (num >= TEN) {
             if (num % TEN == (num / TEN) % TEN) {
-                flag = 1;
+                IsSatisfyingCondition = true;
                 break;
             }
             num /= TEN;
         }
-        if (flag) {
+        if (IsSatisfyingCondition) {
             printf("Yes\n");
         } else {
             printf("No\n");
