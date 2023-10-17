27ead4e73d7b77daa27efa2eec9ce690e21f21c2
Нина Мельникова <melnikova@MacBook-Pro-Nina.local>
2023-10-17 11:27:32 +0300

    readygeo5

--- tasks/geometry/tasks/5/solution.h
+++ tasks/geometry/tasks/5/solution.h
@@ -1,6 +1,61 @@
 #pragma once
-
 #include <stdio.h>
+
+int Mod(int x, int y) {
+    if (y != 0) {
+        return x % y;
+    } else {
+        return 0;
+    }
+}
+
+int Max(int x, int y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int Min(int x, int y) {
+    if (x < y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int Sign(int x) {
+    if (x > 0) {
+        return 1;
+    }
+    if (x < 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+int Abs(int x) {
+    if (x > 0) {
+        return x;
+    } else {
+        return x * (-1);
+    }
+}
+
+const int X_0 = 10;
+const int Y_0 = 10;
+const int R_SMALL = 5;
+const int R_BIG = 10;
+const int I_0 = -1;
+const int J_0 = -1;
+const int L_0 = -9;
+const int ITERATIONS = 50;
+const int TWENTY = 20;
+const int TWENTYFIVE = 25;
+const int THIRTY = 30;
+const int ONE = 1;
 
 typedef struct {
     int x;
@@ -8,11 +63,39 @@ typedef struct {
 } Point;
 
 int CheckZone(Point p) {
-    // Write a code here that checks whether a point belongs to a given area
-    return 0;
+    int cord_x = p.x, cord_y = p.y;
+    if (((cord_x - X_0) * (cord_x - X_0) + (cord_y - Y_0) * (cord_y - Y_0) <= R_BIG * R_BIG) &&
+        ((cord_x - X_0) * (cord_x - X_0) + (cord_y - Y_0) * (cord_y - Y_0) >= R_SMALL * R_SMALL)) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 int Task() {
-    // write main code here
+    int i = I_0;
+    int j = J_0;
+    int l = L_0;
+    Point p;
+    p.x = i;
+    p.y = j;
+
+    int count_belong = 0;  // Счетчик точек, принадлежащих зоне
+
+    for (int k = ONE; k <= ITERATIONS; ++k) {
+        i = Max(p.y - k, l - k) % THIRTY + Max(p.x + l, p.y + k) % TWENTY;
+        j = (Abs(p.x - l) * Sign(p.y + k) + Abs(p.x - k) * (p.y + k)) % TWENTY;
+        l = ((p.x + k) * (p.y - k) * (l + k)) % TWENTYFIVE;
+        p.x = i;
+        p.y = j;
+        printf("k = %d, x = %d, y = %d, l = %d belong = %d \n", k, p.x, p.y, l, CheckZone(p));
+
+        if (CheckZone(p)) {
+            count_belong++;
+        }
+    }
+
+    printf("Number of points belonging to the zone: %d\n", count_belong);
+
     return 0;
 }
