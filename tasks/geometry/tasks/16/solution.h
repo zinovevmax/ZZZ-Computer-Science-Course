8b869ad8dbecc9f1fb92f6cc6e8ab8b6e57df26c
Matvey26 <matvey.ivchenko@yandex.ru>
2023-10-17 21:37:19 +0300

    Fix Mod(), fix Pow() and refactor struct Point

--- tasks/geometry/tasks/16/solution.h
+++ tasks/geometry/tasks/16/solution.h
@@ -3,16 +3,15 @@
 #include <stdio.h>
 
 typedef struct Pnt {
-    int i;
-    int j;
-    int l;
+    int x;
+    int y;
 } Point;
 
 int CheckZone(Point p) {
     // Write a code here that checks whether a point belongs to a given area
     const int b1 = 10;
     const int b2 = 20;
-    return (p.i + p.j + b1 <= 0) && (p.i + p.j + b2 >= 0);
+    return (p.x + p.y + b1 <= 0) && (p.x + p.y + b2 >= 0);
 }
 
 int Abs(int number) {
@@ -23,11 +22,11 @@ int Min(int number_1, int number_2) {
     return (number_1 < number_2) ? number_1 : number_2;
 }
 
-int Max(int number_1, int number_2) {
+int Max(int number_1, int number_2) {   
     return (number_1 > number_2) ? number_1 : number_2;
 }
 
-int Pow(int number, int p) {
+int Pow(int number, unsigned int p) {
     int result = 1;
     while (p--) {
         result *= number;
@@ -37,10 +36,10 @@ int Pow(int number, int p) {
 
 int Mod(int number_1, int number_2) {
     int result = number_1 % number_2;
-    return (result < 0) ? number_2 + result : result;
+    return (result < 0) ? Abs(number_2) + result : result;
 }
 
-const int STEPS = 50;
+const int MAX_ITERATIONS = 50;
 
 const int P1_I = 10;
 const int P2_I = -20;
@@ -53,18 +52,19 @@ const int L0 = 12;
 
 int Task() {
     // start coordinates
-    Point dot = {I0, J0, L0};
-    for (int step = 1; step <= STEPS; ++step) {
-        int i = Abs(dot.i - dot.l) + Min(Mod(dot.j, P1_I), Mod(dot.l * step, P1_I)) + P2_I;
-        int j = Mod(Max(step - dot.i, Min(dot.j, Max(dot.i - dot.l, dot.j - dot.l))), P_J);
-        int l = Mod(Pow(dot.l, 2), P_L) - Mod(Max(dot.i, dot.j), step + 1);
-        dot = {i, j, l};
-
-        printf("Step: %d, Dot: (%d, %d), Is dot in zone: %d\n", step, dot.i, dot.j, CheckZone(dot));
+    Point dot = {I0, J0};
+    int l = L0;
+    for (int step = 1; step <= MAX_ITERATIONS; ++step) {
+        int i = Abs(dot.x - l) + Min(Mod(dot.y, P1_I), Mod(l * step, P1_I)) + P2_I;
+        int j = Mod(Max(step - dot.x, Min(dot.y, Max(dot.x - l, dot.y - l))), P_J);
+        l = Mod(Pow(l, 2), P_L) - Mod(Max(dot.x, dot.y), step + 1);
+        dot = {i, j};
+        printf("Step: %d, Dot: (%d, %d)\n", step, dot.x, dot.y);
         if (CheckZone(dot)) {
-            break;
+            printf("In step #%d the dot enter the area.\n", step);
+            return 0;
         }
     }
-
+    printf("The dot didn't enter the area within 50 steps.\n");
     return 0;
 }
