6586bee19d276f3da991ee3c4b5c5c247b7925b0
Nikita Krushinin <144558570+qwizy@users.noreply.github.com>
2023-10-20 21:40:24 +0300

    GEOMETRY_27_KRUSHININ_NIKITA

--- tasks/geometry/tasks/27/solution.h
+++ tasks/geometry/tasks/27/solution.h
@@ -7,93 +7,67 @@ const int num_1 = 1;
 const float num_001 = 0.01;
 const float num_004 = 0.04;
 
-typedef struct
-{
+typedef struct {
     int x;
     int y;
 } Point;
 
-int CheckZone(Point p)
-{
+int CheckZone(Point p) {
     float x_1 = p.x;
     float y_1 = p.y;
 
-    if (((((x_1 - num_20) * (x_1 - num_20)) * (num_001)) + ((y_1 * y_1) * (num_004))) <= (num_1))
-    {
+    if (((((x_1 - num_20) * (x_1 - num_20)) * (num_001)) + ((y_1 * y_1) * (num_004))) <= (num_1)) {
         return 1;
-    }
-    else
-    {
+    } else {
         return 0;
     }
 }
 
-int Abs(int x)
-{
-    if (x > 0)
-    {
+int Abs(int x) {
+    if (x > 0) {
         return x;
-    }
-    else
-    {
+    } else {
         return x * -1;
     }
 }
 
-int Mod(int x, int y)
-{
-    if (y > 0)
-    {
+int Mod(int x, int y) {
+    if (y > 0) {
         return x % y;
-    }
-    else
-    {
+    } else {
         return y - Abs(x);
     }
 }
 
-int Max(int x, int y)
-{
-    if (x > y)
-    {
+int Max(int x, int y) {
+    if (x > y) {
         return x;
-    }
-    else
-    {
+    } else {
         return y;
     }
 }
 
-int Min(int x, int y)
-{
-    if (x < y)
-    {
+int Min(int x, int y) {
+    if (x < y) {
         return x;
-    }
-    else
-    {
+    } else {
         return y;
     }
 }
 
-int Sign(int x)
-{
-    if (x > 0)
-    {
+int Sign(int x) {
+    if (x > 0) {
         return 1;
     }
-    if (x == 0)
-    {
+    if (x == 0) {
         return 0;
     }
-    if (x < 0)
-    {
+    if (x < 0) {
         return -1;
     }
 }
 
-int main()
-{
+int main() {
     int i_1 = -24;
     int j_1 = 4;
     int l_1 = -3;
@@ -101,8 +75,7 @@ int main()
     int j = j_1;
     int l = l_1;
 
-    for (int k = 0; k < Iterations; k++)
-    {
+    for (int k = 0; k < Iterations; k++) {
         Point p;
         p.x = i;
         p.y = j;
@@ -110,8 +83,7 @@ int main()
         int jk = 0;
         int lk = 0;
 
-        if (CheckZone(p) == 1)
-        {
+        if (CheckZone(p) == 1) {
             printf("x = %d, y = %d, k = %d \n", i, j, k);
             break;
         }
