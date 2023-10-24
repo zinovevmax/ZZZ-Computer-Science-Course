a959ece225948da3fd8760ba42dd3fa5c7e8ce91
xcus33me <harvieorigin@yandex.ru>
2023-10-24 20:02:50 +0300

    321go

--- tasks/integers/tasks/6/solution.h
+++ tasks/integers/tasks/6/solution.h
@@ -5,15 +5,13 @@
 
 int64_t Abs(int64_t a) {
     return (a >= 0) ? a : -a;
-} 
+}
 
 int Task() {
     int64_t number;
     scanf("%lu", &number);
-    
     int64_t first_val;
     int64_t difference;
-
     while (number >= 100) {
         first_val = number % 10;
         difference = Abs((number / 100 % 10) - (number % 100 / 10));
