88f5d448b7e72c3a33f4996819ae59a6ea866439
xcus33me <harvieorigin@yandex.ru>
2023-10-24 20:13:20 +0300

    321go

--- tasks/integers/tasks/6/solution.h
+++ tasks/integers/tasks/6/solution.h
@@ -3,22 +3,26 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// some black magic (*^.^*)
+const int64_t MAGIC_HUNNID = 100;
+const int64_t MAGIC_TEN = 10;
+
 int64_t Abs(int64_t a) {
     return (a >= 0) ? a : -a;
 }
 
 int Task() {
-    int64_t number;
+    int64_t number = 0;
     scanf("%lu", &number);
-    int64_t first_val;
-    int64_t difference;
-    while (number >= 100) {
-        first_val = number % 10;
-        difference = Abs((number / 100 % 10) - (number % 100 / 10));
+    int64_t first_val = 0;
+    int64_t difference = 0;
+    while (number >= MAGIC_HUNNID) {
+        first_val = number % MAGIC_TEN;
+        difference = Abs((number / MAGIC_HUNNID % MAGIC_TEN) - (number % MAGIC_HUNNID / MAGIC_TEN));
         if (first_val == difference) {
-            printf("%lu %lu %lu\n", (number / 100 % 10), (number % 100 / 10), first_val);
+            printf("%lu %lu %lu\n", (number / MAGIC_HUNNID % MAGIC_TEN), (number % MAGIC_HUNNID / MAGIC_TEN), first_val);
         }
-        number = number / 10;
+        number = number / MAGIC_TEN;
     }
     return 0;
 }
