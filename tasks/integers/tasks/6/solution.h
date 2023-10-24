d66ffb81f14e97fe80871c6957565c77d75a572f
xcus33me <harvieorigin@yandex.ru>
2023-10-24 19:43:53 +0300

    321go

--- tasks/integers/tasks/6/solution.h
+++ tasks/integers/tasks/6/solution.h
@@ -3,7 +3,24 @@
 #include <stdint.h>
 #include <stdio.h>
 
+int64_t Abs(int64_t a) {
+    return (a >= 0) ? a : -a;
+} 
+
 int Task() {
-    // write your solution here
+    int64_t number;
+    scanf("%lu", &number);
+    
+    int64_t first_val;
+    int64_t difference;
+
+    while (number >= 100) {
+        first_val = number % 10;
+        difference = Abs((number / 100 % 10) - (number % 100 / 10));
+        if (first_val == difference) {
+            printf("%lu %lu %lu\n", (number / 100 % 10), (number % 100 / 10), first_val);
+        }
+        number = number / 10;
+    }
     return 0;
 }
