64cc57439f6dc2c6960649baa79fb2aef93e571b
Нина Мельникова <melnikova@MacBook-Pro-Nina.local>
2023-10-25 13:24:02 +0300

    19

--- tasks/integers/tasks/19/solution.h
+++ tasks/integers/tasks/19/solution.h
@@ -4,31 +4,32 @@
 #include <stdio.h>
 
 const int64_t NUMBER = 15551;  // comment this line
+const int64_t TEN = 10;
 
 int Task() {
-    // scanf("%lld", &number); // uncomment this line
+    // scanf("%ld", &number); // uncomment this line
     int64_t number = NUMBER;  // comment this line
 
     if (number < 0) {
         number = -number;  // Обработка отрицательных чисел
     }
 
-    if (number < 10) {
-        printf("%lld\n", number);
+    if (number < TEN) {
+        printf("%ld\n", number);
         return 0;  // Числа до трех разрядов без изменений
     }
 
     // Отсекаем последнюю цифру
-    number /= 10;
+    number /= TEN;
 
     // Отсекаем первую цифру
     int64_t number_of_digits = 1;
-    while (number / (number_of_digits * 10) > 0) {
-        number_of_digits *= 10;
+    while (number / (number_of_digits * TEN) > 0) {
+        number_of_digits *= TEN;
     }
     number %= number_of_digits;
 
-    printf("%lld\n", number);
+    printf("%ld\n", number);
 
     return 0;
 }
