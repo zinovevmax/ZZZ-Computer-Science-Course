3622b5dcb202fa0dd62b100125a3b64bf715597f
DmkitryNai <naipakdmitry@yandex.ru>
2023-10-25 23:26:44 +0300

    INTEGERS_VAR25_DmitryNaypak

--- tasks/integers/tasks/25/solution.h
+++ tasks/integers/tasks/25/solution.h
@@ -3,7 +3,34 @@
 #include <stdint.h>
 #include <stdio.h>
 
+const int64_t INPUT_NUMBER = 12345;
+const int8_t ONE = 1;
+const int32_t TEN = 10;
+
+// Function of exponentiation
+int Power(int32_t start_number, int32_t y) {
+    int32_t i = 0;
+    int32_t result = 1;
+    for (i = 1; i <= y; ++i) {
+        result *= start_number;
+    }
+    return result;
+}
+
 int Task() {
-    // write your solution here
+    int32_t count = 0;
+    int64_t number = INPUT_NUMBER;
+    while (number) {
+        number /= TEN;
+        ++count;
+    }
+    number = INPUT_NUMBER;
+    for (uint16_t k = count; k >= ONE; --k) {
+        int64_t digit_number = (number / (Power(TEN, (k - 1))) % Power(TEN, 1));
+        if (digit_number % 2 == 0) {
+            --digit_number;
+        }
+        printf("%ld", digit_number);
+    }
     return 0;
 }
