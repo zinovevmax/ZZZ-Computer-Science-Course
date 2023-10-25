3ee571ee5bba3160267cf236e9a6dbbbcbb23a53
Нина Мельникова <melnikova@MacBook-Pro-Nina.local>
2023-10-25 13:14:21 +0300

    19

--- tasks/integers/tasks/19/solution.h
+++ tasks/integers/tasks/19/solution.h
@@ -3,7 +3,32 @@
 #include <stdint.h>
 #include <stdio.h>
 
+const int64_t NUMBER = 15551;  // comment this line
+
 int Task() {
-    // write your solution here
+    // scanf("%lld", &number); // uncomment this line
+    int64_t number = NUMBER;  // comment this line
+
+    if (number < 0) {
+        number = -number;  // Обработка отрицательных чисел
+    }
+
+    if (number < 10) {
+        printf("%lld\n", number);
+        return 0;  // Числа до трех разрядов без изменений
+    }
+
+    // Отсекаем последнюю цифру
+    number /= 10;
+
+    // Отсекаем первую цифру
+    int64_t number_of_digits = 1;
+    while (number / (number_of_digits * 10) > 0) {
+        number_of_digits *= 10;
+    }
+    number %= number_of_digits;
+
+    printf("%lld\n", number);
+
     return 0;
 }
