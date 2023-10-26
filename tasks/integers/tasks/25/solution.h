14f5cf3b83f75aae9e5e77192aca3520d390191d
DmitryNai <144270336+DmitryNai@users.noreply.github.com>
2023-10-26 17:36:48 +0300

    Update solution.h

--- tasks/integers/tasks/25/solution.h
+++ tasks/integers/tasks/25/solution.h
@@ -27,19 +27,19 @@ int Task() {
     number = INPUT_NUMBER;
     if (INPUT_NUMBER >= 0) {
         for (uint16_t k = count; k >= ONE; --k) {
-        int64_t digit_number = (number / (Power(TEN, (k - 1))) % Power(TEN, 1));
-        if (digit_number % 2 == 0) {
-            --digit_number;
-        }
+            int64_t digit_number = (number / (Power(TEN, (k - 1))) % Power(TEN, 1));
+            if (digit_number % 2 == 0) {
+                --digit_number;
+            }
             printf("%ld", digit_number);
         }
     } else {
         printf("%s", "-");
         for (uint16_t k = count; k >= ONE; --k) {
-        int64_t digit_number = -(number / (Power(TEN, (k - 1))) % Power(TEN, 1));
-        if (digit_number % 2 == 0) {
-            --digit_number;
-        }
+            int64_t digit_number = -(number / (Power(TEN, (k - 1))) % Power(TEN, 1));
+            if (digit_number % 2 == 0) {
+                --digit_number;
+            }
             printf("%ld", digit_number);
         }
     }
