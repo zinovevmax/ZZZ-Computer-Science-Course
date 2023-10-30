5c71530dd36787578ea467ccb60bbbf39bbb1116
DmkitryNai <naipakdmitry@yandex.ru>
2023-10-30 20:28:06 +0300

    BITSETS_VAR12_DmitryNaypak

--- tasks/bitsets/tasks/12/solution.h
+++ tasks/bitsets/tasks/12/solution.h
@@ -5,7 +5,29 @@
 
 typedef uint32_t Bitset;
 
+const uint32_t CONSONANT = 4277124846u;  // number for all consonants
+
 int Task() {
-    // write your solution here
+    Bitset word = 0u;
+    Bitset consonant_for_compare = CONSONANT;
+    char writing = ' ';
+    char imput_words[] = "mama myla ramu 0";
+    uint32_t counter_words = 0;
+
+    while (imput_words[counter_words] != '0') {  // replacing with "scanf" when we output from console
+        writing = imput_words[counter_words];    // delete if "scanf"
+        if (writing >= 'a' && writing <= 'z') {
+            word = word | (1u << (writing - 'a'));
+        } else {
+            consonant_for_compare = word & consonant_for_compare;
+            word = 0u;
+        }
+        ++counter_words;
+    }
+    if (consonant_for_compare != 0u) {
+        printf("Yes, words have a common consonant");
+    } else {
+        printf("No, words do not have a common consonant");
+    }
     return 0;
 }
