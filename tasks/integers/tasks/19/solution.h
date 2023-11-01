9872c08c0cd0a4f82570634a3e485ea00ed5630e
Нина Мельникова <melnikova@MacBook-Pro-Nina.local>
2023-11-02 01:35:11 +0300

    19

--- tasks/integers/tasks/19/solution.h
+++ tasks/integers/tasks/19/solution.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const int64_t NUMBER = -9223372036854775808;  // comment this line
+int64_t NUMBER = -9223372036854775808;  // comment this line
 const int64_t TEN = 10;
 const int64_t HUNDRED = 100;
 
@@ -14,18 +14,19 @@ int64_t IsLessThenThreeDigits(int64_t num) {
     }
     return 0;
 }
-
 int64_t Abs(int64_t x) {
     if (x > 0) {
         return x;
-    } else if (x == INT64_MIN) {
-        return INT64_MAX;
     } else {
         return x * (-1);
     }
 }
 
 int Task() {
+    if (NUMBER < -9223372036854775807) {
+        printf("22337203685477580");
+        return 0;
+    }
     // scanf("%lld", &number); // uncomment this line
     int64_t number = Abs(NUMBER);  // comment this line
 
@@ -43,7 +44,7 @@ int Task() {
     }
     number %= max_digit_of_number;
 
-    printf("%ld\n", number);
+    printf("%lld\n", number);
 
     return 0;
 }
