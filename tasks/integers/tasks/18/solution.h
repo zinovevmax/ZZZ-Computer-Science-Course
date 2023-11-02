9aeb8b221738288eebc004f46bdb20fa358bf831
mashulyag <ya.merri2005@yandex.ru>
2023-11-02 13:21:17 +0300

    lalala

--- tasks/integers/tasks/18/solution.h
+++ tasks/integers/tasks/18/solution.h
@@ -9,25 +9,25 @@ const int64_t EXAMPLE = 1234;
 
 int Task() {
     // write your solution here
-    int64_t number_with_znak = EXAMPLE;
-    int64_t znak = 0;
-    if (number_with_znak < 0) {
-        znak = -1;
+    int64_t number_with_sign = EXAMPLE;
+    int64_t sign = 0;
+    if (number_with_sign < 0) {
+        sign = -1;
     } else {
-        znak = 1;
+        sign = 1;
     }
     int64_t number = abs(EXAMPLE);
     int64_t copy_number = abs(EXAMPLE);
     int64_t second = 0;
     int64_t penultimate = (number / BASE) % BASE;
-    int64_t n = 1;
+    int64_t position_of_second = 1;
     while (copy_number > BASE) {
         second = copy_number % BASE;
         copy_number = copy_number / BASE;
-        n = n * BASE;
+        position_of_second = position_of_second * BASE;
     }
-    n = n / BASE;
-    int64_t new_number = (number - second * n - penultimate * BASE + penultimate * n + second * BASE) * znak;
+    position_of_second = position_of_second / BASE;
+    int64_t new_number = (number - second * position_of_second - penultimate * BASE + penultimate * position_of_second + second * BASE) * sign;
     printf("%ld/n", new_number);
     return 0;
 }
