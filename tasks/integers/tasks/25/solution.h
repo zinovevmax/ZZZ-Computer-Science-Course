c58000797f0d220f6cef233ba410edfcc3bb91f0
DmitryNai <144270336+DmitryNai@users.noreply.github.com>
2023-11-02 11:37:58 +0300

    Update solution.h

--- tasks/integers/tasks/25/solution.h
+++ tasks/integers/tasks/25/solution.h
@@ -1,8 +1,13 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const int64_t INPUT_NUMBER = -402345;
+const int64_t INPUT_NUMBER = -9223;
 const int32_t TEN = 10;
+const int32_t MINUS_ONE = -1;
+
+int Abs(int num) {
+    return (num >= 0) ? num : num * MINUS_ONE;
+}
 
 // Function of exponentiation
 int Power(int32_t start_number, uint32_t degree) {
@@ -15,6 +20,7 @@ int Power(int32_t start_number, uint32_t degree) {
 }
 
 int Task() {
+    // write your solution here
     int32_t count_of_index = 0;
     int64_t input_number = INPUT_NUMBER;
     while (input_number) {
@@ -22,29 +28,19 @@ int Task() {
         ++count_of_index;
     }
     input_number = INPUT_NUMBER;
-    if (INPUT_NUMBER >= 0) {
-        for (uint16_t k = count_of_index; k >= 1; --k) {
+    for (uint16_t k = count_of_index; k >= 1; --k) {
+        if (k == count_of_index && input_number < 0) {
             int64_t digit = (input_number / (Power(TEN, (k - 1))) % Power(TEN, 1));
+            if (digit % 2 == 0 && digit != 0) {
+                ++digit;
+            }
+            printf("%ld", digit);
+        } else {
+            int64_t digit = Abs((input_number / (Power(TEN, (k - 1))) % Power(TEN, 1)));
             if (digit % 2 == 0 && digit != 0) {
                 --digit;
             }
             printf("%ld", digit);
-        }
-    } else {
-        for (uint16_t k = count_of_index; k >= 1; --k) {
-            if (k == count_of_index) {
-                int64_t digit = (input_number / (Power(TEN, (k - 1))) % Power(TEN, 1));
-                if (digit % 2 == 0 && digit != 0) {
-                    ++digit;
-                }
-                printf("%ld", digit);
-            } else {
-                int64_t digit = -(input_number / (Power(TEN, (k - 1))) % Power(TEN, 1));
-                if (digit % 2 == 0 && digit != 0) {
-                    --digit;
-                }
-                printf("%ld", digit);
-            }
         }
     }
     return 0;
