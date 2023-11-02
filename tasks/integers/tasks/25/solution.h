397d40300fb90c442c889255b3fc2b6748595603
DmitryNai <144270336+DmitryNai@users.noreply.github.com>
2023-11-02 13:51:22 +0300

    Update solution.h

--- tasks/integers/tasks/25/solution.h
+++ tasks/integers/tasks/25/solution.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const int64_t INPUT_NUMBER = -9223;
+const int64_t INPUT_NUMBER = -922337203685477580;
 const int32_t TEN = 10;
 const int64_t MINUS_ONE = -1;
 
@@ -13,39 +13,33 @@ int64_t Abs(int64_t num) {
     }
 }
 
-// Function of exponentiation
-int Power(int32_t start_number, uint32_t degree) {
-    uint32_t i = 0;
-    int32_t result = 1;
-    for (i = 1; i <= degree; ++i) {
-        result *= start_number;
+int64_t Sign(int64_t num) {
+    if (num > 0) {
+        return 1;
+    } else if (num < 0) {
+        return -1;
+    } else {
+        return 0;
     }
-    return result;
 }
 
-int Task() {
-    // write your solution here
-    int32_t count_of_index = 0;
+int main()
+{   
     int64_t input_number = INPUT_NUMBER;
-    while (input_number) {
-        input_number /= TEN;
-        ++count_of_index;
-    }
-    input_number = INPUT_NUMBER;
-    for (uint16_t k = count_of_index; k >= 1; --k) {
-        if (k == count_of_index && input_number < 0) {
-            int64_t digit = (input_number / (Power(TEN, (k - 1))) % Power(TEN, 1));
-            if (digit % 2 == 0 && digit != 0) {
-                ++digit;
-            }
-            printf("%ld", digit);
-        } else {
-            int64_t digit = Abs((input_number / (Power(TEN, (k - 1))) % Power(TEN, 1)));
-            if (digit % 2 == 0 && digit != 0) {
-                --digit;
-            }
-            printf("%ld", digit);
+    int8_t is_negative = Sign(input_number);
+    input_number = Abs(input_number);
+    int64_t new_number = 0;
+    int64_t power_10 = 1;
+    
+    while (input_number >= power_10) {
+        int64_t digit = (input_number / power_10) % 10;
+        if (digit) {
+            digit -= (int64_t)(digit % 2 == 0);
         }
+        new_number += digit * power_10;
+        power_10 *= 10l;
     }
+    new_number *= (int64_t)is_negative;
+    printf("%ld\n", new_number);
     return 0;
 }
