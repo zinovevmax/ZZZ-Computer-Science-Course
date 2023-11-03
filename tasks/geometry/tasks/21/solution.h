147923e277a8cc6d0194c581554708833b98e36e
gleb <bleb_1@mail.ru>
2023-11-03 16:03:52 +0300

    geometry

--- tasks/geometry/tasks/21/solution.h
+++ tasks/geometry/tasks/21/solution.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include <stdio.h>
-const int ELEVEN = 11;
-const int MINUS_TWELVE = -12;
-const int MINUS_TWENTY_TWO = -22;
+const int INITIAL_L = 11;
+const int INITIAL_I = -12;
+const int INITIAL_J = -22;
 const int TWENTY = 20;
 const int TEN = 10;
 const int MINUS_TEN = -10;
 const int MINUS_ONE = -1;
-const int MAX_ITERATION = 50;
+const int MAX_ITERATIONS = 50;
 
 typedef struct {
     int x;
@@ -24,18 +24,22 @@ int Abs(int n) {
 
 int Mod(int a, int b) {
     int res = 0;
-    if (a >= 0) {
-        while (a >= b) {
-            a = a - b;
+    if (b != 0) {
+        if (a >= 0) {
+            while (a >= b) {
+                a = a - b;
+            }
+            res = a;
+        } else {
+            while (a <= res) {
+                res = res - Abs(b);
+            }
+            res = a - res;
         }
-        res = a;
+        return res;
     } else {
-        while (a <= res) {
-            res = res - Abs(b);
-        }
-        res = a - res;
+        return 0;
     }
-    return res;
 }
 
 int Min(int a, int b) {
@@ -64,41 +68,36 @@ int Sign(int a) {
 
 int CheckZone(Point p) {
     // Write a code here that checks whether a point belongs to a given area
-    int is_local_success = 0;
     if (p.y >= p.x + TEN && p.y <= MINUS_ONE * p.x + TEN && p.x >= MINUS_TEN) {
         printf("Точка с координатами %d %d попадает в область\n", p.x, p.y);
-        is_local_success = 1;
     } else {
         printf("Точка с координатами %d %d НЕ попадает в область\n", p.x, p.y);
     }
-    return is_local_success;
+    return (p.y >= p.x + TEN && p.y <= MINUS_ONE * p.x + TEN && p.x >= MINUS_TEN);
 }
 
 int Task() {
     // write main code here
     Point p;
-    p.x = MINUS_TWELVE;
-    p.y = MINUS_TWENTY_TWO;
-    int l = ELEVEN;
-    int is_global_success = 0;
+    p.x = INITIAL_I;
+    p.y = INITIAL_J;
+    int l = INITIAL_L;
+    int is_success = 0;
     int temp_x = 0;
     int temp_y = 0;
-    int temp_l = 0;
-    for (int i = 0; i < MAX_ITERATION; ++i) {
-        if (CheckZone(p) == 1) {
-            is_global_success = 1;
+    for (int i = 0; i < MAX_ITERATIONS; ++i) {
+        if (CheckZone(p)) {
+            is_success = 1;
             break;
         }
         temp_x = p.x;
         temp_y = p.y;
-        temp_l = l;
-        p.x = Max(Mod(Min(temp_x - temp_y, temp_y - temp_l), TWENTY), Mod(Min(temp_x - temp_l, temp_y - i), TWENTY)) +
-              TEN;
+        p.x = Max(Mod(Min(temp_x - temp_y, temp_y - l), TWENTY), Mod(Min(temp_x - l, temp_y - i), TWENTY)) + TEN;
         p.y = Sign(temp_x - temp_y) * Min(Mod(temp_x, TWENTY), Mod(temp_y, TWENTY)) -
-              Mod(Max(Abs(temp_x - temp_l), Abs(i - TWENTY)), TWENTY) + TWENTY;
-        l = Mod(temp_x, TEN) * Mod(temp_y, TEN) + Mod(temp_l, TEN);
+              Mod(Max(Abs(temp_x - l), Abs(i - TWENTY)), TWENTY) + TWENTY;
+        l = Mod(temp_x, TEN) * Mod(temp_y, TEN) + Mod(l, TEN);
     }
-    if (!is_global_success) {
+    if (!is_success) {
         printf("Точка не попала в область за 50 итераций.\n");
     }
     return 0;
