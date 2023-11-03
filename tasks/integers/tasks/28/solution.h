f517065b5b25a667ee3a9a30828fb82793491f92
gleb <bleb_1@mail.ru>
2023-11-03 16:17:04 +0300

    int2

--- tasks/integers/tasks/28/solution.h
+++ tasks/integers/tasks/28/solution.h
@@ -3,23 +3,30 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const int TEN = 10;
+int32_t CheckOnMinus(int32_t number) {
+    if (number < 0) {
+        number *= -1;
+        printf("-");
+    }
+    return number;
+}
+
+int64_t Convertation(int32_t decimal_number) {
+    int64_t multyplier = 1;
+    int64_t binary_number = 0;
+    while (decimal_number > 0) {
+        binary_number += (decimal_number % 2) * multyplier;
+        decimal_number /= 2;
+        multyplier *= 10;
+    }
+    return binary_number;
+}
 
 int Task() {
     // write your solution here
-    int32_t number_in_10 = 0;
-    int64_t number_in_2 = 0;
-    int64_t multyplier = 1;
-    scanf("%d", &number_in_10);
-    if (number_in_10 < 0) {
-        number_in_10 *= -1;
-        printf("-");
-    }
-    while (number_in_10 > 0) {
-        number_in_2 += (number_in_10 % 2) * multyplier;
-        number_in_10 /= 2;
-        multyplier *= TEN;
-    }
-    printf("%ld\n", number_in_2);
+    int32_t decimal_number = 0;
+    scanf("%d", &decimal_number);
+    decimal_number = CheckOnMinus(decimal_number);
+    printf("%ld\n", Convertation(decimal_number));
     return 0;
 }
