293749aa9dd1d010e47865bd15bc301472eeed62
xcus33me <harvieorigin@yandex.ru>
2023-11-03 17:20:34 +0300

    test

--- tasks/bitsets/tasks/25/solution.h
+++ tasks/bitsets/tasks/25/solution.h
@@ -1,11 +1,45 @@
 #pragma once
 
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
 typedef uint32_t Bitset;
 
+// Sibilant consonants:
+// sh, ch, zh, th
+
+Bitset sibilantConsonants = 0;
+
+bool CheckForSibilantConsonants(char c) {
+    c = tolower(c);
+    if ((c == 's') || (c == 'c') || (c == 'h') || (c == 't') || (c == 'z')) {
+        if (sibilantConsonants == 0) {
+            if (c != 'h')
+                sibilantConsonants |= (1u << (c - 'a'));
+            return false;
+        }
+        else {
+            if (c == 'h')
+                return true;
+        }
+    }
+    else {
+        sibilantConsonants = 0;
+        return false;
+    }
+}
+
 int Task() {
-    // write your solution here
+    char input_letter;
+    while ((input_letter = (char)getchar()) != EOF)
+    {
+        bool answer = CheckForSibilantConsonants(input_letter);
+        if (answer) {
+            printf("A word consisting of at least one sibilant was detected!");
+            break;
+        }
+    }
     return 0;
 }
