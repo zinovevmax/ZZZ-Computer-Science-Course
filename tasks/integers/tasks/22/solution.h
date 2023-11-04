3e1733d8460063f3966ec89c2fb80fbc263781e1
JDorof <demadorofeevicok977@gmail.com>
2023-11-04 19:56:18 +0300

    adding functions

--- tasks/integers/tasks/22/solution.h
+++ tasks/integers/tasks/22/solution.h
@@ -3,37 +3,51 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int Task() {
-    int64_t number = 0;
-    int64_t reversed_result = 0;
-    int64_t result = 0;
-    int64_t digit = 0;
-    int64_t sign_of_number = 0;
-    scanf("%li", &number);
+int64_t Sign(int64_t number) {
     if (number < 0) {
-        sign_of_number = -1;
-        number = -number;
+        return -1;
     } else if (number > 0) {
-        sign_of_number = 1;
+        return 1;
     } else {
-        printf("%li", number);
         return 0;
     }
+}
+
+int64_t Delete_even_digits(int64_t number) {
+    int64_t reversed_result = 0;
+    int64_t digit = 0;
     while (number > 0) {
         digit = number % 10;
         number /= 10;
-        if (digit % 2 == 0) {
+        if (digit % 2 != 0) {
             reversed_result *= 10;
             reversed_result += digit;
         }
     }
-    while (reversed_result > 0) {
-        digit = reversed_result % 10;
-        reversed_result /= 10;
+    return reversed_result;
+}
+
+int64_t Reverse(int64_t number_to_reverse) {
+    int64_t result = 0;
+    int64_t digit = 0;
+    while (number_to_reverse > 0) {
+        digit = number_to_reverse % 10;
+        number_to_reverse /= 10;
         result *= 10;
         result += digit;
     }
-    result = sign_of_number * result;
-    printf("%li", result);
+    return result;
+}
+
+int Task() {
+    int64_t number = 0;
+    int64_t sign_of_number = 0;
+    scanf("%li", &number);
+    sign_of_number = Sign(number);
+    number *= sign_of_number;
+    number = Delete_even_digits(number);
+    number = Reverse(number);
+    number *= sign_of_number;
+    printf("%li", number);
     return 0;
 }
