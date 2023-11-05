4459bcbdaad2d765d682a22e7d55a8e48d29e7c5
mashulyag <ya.merri2005@yandex.ru>
2023-11-05 19:52:57 +0300

    hello

--- tasks/integers/tasks/18/solution.h
+++ tasks/integers/tasks/18/solution.h
@@ -6,6 +6,7 @@
 
 const int64_t BASE = 10;
 const int64_t EXAMPLE = 1234;
+const int64_t MIN_OR_MAX_INT_WITHOUT_SIGN = 9223372036854775808;
 
 int Task() {
     // write your solution here
@@ -16,20 +17,25 @@ int Task() {
     } else {
         sign = 1;
     }
-    int64_t number = abs(EXAMPLE);
-    int64_t copy_number = abs(EXAMPLE);
-    int64_t second = 0;
-    int64_t penultimate = (number / BASE) % BASE;
-    int64_t position_of_second = 1;
-    while (copy_number > BASE) {
-        second = copy_number % BASE;
-        copy_number = copy_number / BASE;
-        position_of_second = position_of_second * BASE;
+    if (EXAMPLE == MIN_OR_MAX_INT_WITHOUT_SIGN) {
+        int64_t new_int64_with_permutations = 9023372036854775828;
+        printf("%ld\n", new_int64_with_permutations * sign);
+    } else {
+        int64_t number = abs(EXAMPLE);
+        int64_t copy_number = abs(EXAMPLE);
+        int64_t second = 0;
+        int64_t penultimate = (number / BASE) % BASE;
+        int64_t position_of_second = 1;
+        while (copy_number > BASE) {
+            second = copy_number % BASE;
+            copy_number = copy_number / BASE;
+            position_of_second = position_of_second * BASE;
+        }
+        position_of_second = position_of_second / BASE;
+        int64_t new_number = (number - second * position_of_second - penultimate * BASE +
+                              penultimate * position_of_second + second * BASE) *
+                             sign;
+        printf("%ld\n", new_number);
     }
-    position_of_second = position_of_second / BASE;
-    int64_t new_number = 
-    (number - second * position_of_second - penultimate * BASE + penultimate * position_of_second + second * BASE) *
-    sign;
-    printf("%ld\n", new_number);
     return 0;
 }
