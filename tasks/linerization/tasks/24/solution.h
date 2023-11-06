fe439e881e9999415b03e7301d8ef8b618e4e01b
Matvey26 <matvey.ivchenko@yandex.ru>
2023-11-06 17:29:04 +0300

    Bugfix

--- tasks/linerization/tasks/24/solution.h
+++ tasks/linerization/tasks/24/solution.h
@@ -12,7 +12,7 @@ typedef struct {
     size_t end;
 } Slice;
 
-#define isLess(x, y) ((y >= x) ? 1 : 0)
+#define Sign(x) ((x >= 0) ? 1 : -1)
 
 void Swap(size_t* n, size_t* m) {
     size_t t = *n;
@@ -20,50 +20,54 @@ void Swap(size_t* n, size_t* m) {
     *m = t;
 }
 
-// Заполняет массив числами по спирали, начиная с левого нижнего угла и двигаясь по часовой стрелке
-void Fill(Matrix matrix, size_t n, size_t m) {
-    int32_t element = 1;
+// Заполняет матрицу входными значениями
+void Fill(Matrix matrix) {
+    matrix[0][0] = 4;
+    matrix[0][1] = 5;
+    matrix[0][2] = 6;
+    matrix[0][3] = 7;
+    matrix[1][0] = 3;
+    matrix[1][1] = 14;
+    matrix[1][2] = 15;
+    matrix[1][3] = 8;
+    matrix[2][0] = 2;
+    matrix[2][1] = 13;
+    matrix[2][2] = 16;
+    matrix[2][3] = 9;
+    matrix[3][0] = 1;
+    matrix[3][1] = 12;
+    matrix[3][2] = 11;
+    matrix[3][3] = 10;
+}
+
+// Линеризует матрицу, обходя её по спирали, начиная с левого нижнего угла (по часовой стрелке)
+void Linerization(Matrix matrix, Row lin_matrix, size_t n, size_t m) {
+    size_t element = 0;
 
     Slice y_axis = {n - 1, 0};
     Slice x_axis = {m - 1, 0};
 
     int32_t is_horizontal = 0;
-    int32_t step = 0;
+    int32_t step = Sign((int32_t)y_axis.end - (int32_t)y_axis.start);
 
-    while (element <= (int32_t)(n * m)) {
+    while (element < n * m) {
         if (is_horizontal) {
-            step = 2 * isLess(x_axis.start, x_axis.end) - 1;
             for (size_t j = x_axis.start; j != x_axis.end + step; j += step) {
-                if (matrix[y_axis.end][j] != 0) {
-                    break;
-                }
-                matrix[y_axis.end][j] = element++;
+                lin_matrix[element++] = matrix[y_axis.end][j];
             }
-
             Swap(&y_axis.start, &y_axis.end);
-            y_axis.start += 2 * isLess(y_axis.start, y_axis.end) - 1;
+            step = Sign((int32_t)y_axis.end - (int32_t)y_axis.start);
+            y_axis.start += step;
         } else {
-            step = 2 * isLess(y_axis.start, y_axis.end) - 1;
             for (size_t i = y_axis.start; i != y_axis.end + step; i += step) {
-                if (matrix[i][x_axis.end] != 0) {
-                    break;
-                }
-                matrix[i][x_axis.end] = element++;
+                lin_matrix[element++] = matrix[i][x_axis.end];
             }
-
             Swap(&x_axis.start, &x_axis.end);
-            x_axis.start += 2 * isLess(x_axis.start, x_axis.end) - 1;
+            step = Sign((int32_t)x_axis.end - (int32_t)x_axis.start);
+            x_axis.start += step;
         }
 
         is_horizontal = !is_horizontal;
-    }
-}
-
-void Linerization(Matrix matrix, Row lin_matrix, size_t n, size_t m) {
-    for (size_t i = 0; i < n; ++i) {
-        for (size_t j = 0; j < m; ++j) {
-            lin_matrix[i * m + j] = matrix[i][j];
-        }
     }
 }
 
@@ -83,32 +87,30 @@ void PrintRow(Row row, size_t n) {
     printf("\n");
 }
 
-const size_t N = 6;
-const size_t M = 9;
+const size_t N = 4;
+const size_t M = 4;
 
 int Task() {
-    size_t n = N;
-    size_t m = M;
-    // scanf("%lu%lu", &n, &m);
-
     // Инициализируем матрицу
-    Row lin_matrix = (Row)calloc(n * m, sizeof(Row));
-    Matrix matrix = (Matrix)calloc(n, sizeof(Row));
-    for (size_t i = 0; i < n; ++i) {
-        matrix[i] = (Row)calloc(m, sizeof(Row));
+    Row lin_matrix = (Row)calloc(N * M, sizeof(Row));
+    Matrix matrix = (Matrix)calloc(N, sizeof(Row));
+    for (size_t i = 0; i < N; ++i) {
+        matrix[i] = (Row)calloc(M, sizeof(Row));
     }
 
+    // Заполняем матрицу
+    Fill(matrix);
+
     // Решаем задачу
-    Fill(matrix, n, m);
-    Linerization(matrix, lin_matrix, n, m);
+    Linerization(matrix, lin_matrix, N, M);
 
     // Выводим результат
-    PrintMatrix(matrix, n, m);
+    PrintMatrix(matrix, N, M);
     printf("\n");
-    PrintRow(lin_matrix, n * m);
+    PrintRow(lin_matrix, N * M);
 
     // Освобождаем память
-    for (size_t i = 0; i < n; ++i) {
+    for (size_t i = 0; i < N; ++i) {
         free(matrix[i]);
     }
     free(matrix);
