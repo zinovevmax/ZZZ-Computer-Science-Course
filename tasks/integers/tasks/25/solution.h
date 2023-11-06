3497b3f7cbcf0f2164588d73cc1a1cd7dbec36ef
DmitryNai <144270336+DmitryNai@users.noreply.github.com>
2023-11-06 16:47:00 +0300

    Update solution.h

--- tasks/integers/tasks/25/solution.h
+++ tasks/integers/tasks/25/solution.h
@@ -4,12 +4,10 @@
 #include <stdio.h>
 
 const int64_t INPUT_NUMBER = -12345;
-const int32_t TEN = 10;
-const int64_t MINUS_ONE = -1;
 
 int64_t Abs(int64_t num) {
     if (num < 0) {
-        return num *= MINUS_ONE;
+        return num *= -1;
     } else {
         return num;
     }
@@ -25,16 +23,17 @@ int64_t Sign(int64_t num) {
     }
 }
 
-int64_t Solve(int64_t number) {
+int64_t DecrementOfEvenNumbers(int64_t number) {
     int64_t new_number = 0;
-    int64_t power_10 = 1;
-    while (number >= power_10) {
-        int64_t digit = (number / power_10) % TEN;
+    int64_t value_power_of_ten = 1;
+    int64_t digit = 0;
+    while (number >= value_power_of_ten) {
+        digit = (number / value_power_of_ten) % 10;
         if (digit) {
-            digit -= (int64_t)(digit % 2 == 0);
+            digit -= (digit % 2 == 0);
         }
-        new_number += digit * power_10;
-        power_10 *= TEN;
+        new_number += digit * value_power_of_ten;
+        power_10 *= 10;
     }
     return new_number;
 }
@@ -42,11 +41,11 @@ int64_t Solve(int64_t number) {
 int Task() {
     int64_t input_number = INPUT_NUMBER;
     if (input_number == INT64_MIN) {
-        printf("-%ld7\n", Solve(input_number / -TEN));
+        printf("-%ld7\n", DecrementOfEvenNumbers(input_number / -10));
     } else {
-        int64_t is_negative = Sign(input_number);
+        int64_t sign_of_number = Sign(input_number);
         input_number = Abs(input_number);
-        printf("%ld\n", is_negative * Solve(input_number));
+        printf("%ld\n", sign_of_number * DecrementOfEvenNumbers(input_number));
     }
     return 0;
 }
