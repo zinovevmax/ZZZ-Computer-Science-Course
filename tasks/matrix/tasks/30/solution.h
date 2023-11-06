1bf90b649899f96516847537f72ab5dd30c91906
winterfulllll <masterfm65@gmail.com>
2023-11-06 23:45:18 +0300

    mat30_v2

--- tasks/matrix/tasks/30/solution.h
+++ tasks/matrix/tasks/30/solution.h
@@ -27,10 +27,6 @@ void FreeMatrix(Matrix matrix, int32_t n) {
 // Функция для инициализации матрицы и её размера из файла
 Matrix InitializeMatrixFromFile(const char* filename, int32_t* n) {
     FILE* file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("File opening error\n");
-        return 0;
-    }
     fscanf(file, "%d", n);
     Matrix matrix = CreateMatrix(*n);
     for (int i = 0; i < *n; i++) {
@@ -42,12 +38,13 @@ Matrix InitializeMatrixFromFile(const char* filename, int32_t* n) {
     return matrix;
 }
 
-// Функция основного алгоритма
+// Функция преобразования матрицы
 Matrix MatrixWithModifiedCols(Matrix matrix, int32_t n) {
     int32_t min_value = INT32_MAX;
     int32_t max_value = INT32_MIN;
     int32_t last_col_with_max_value = -1;
-    // Проходим по столбцам
+
+    // Проходим по столбцам, ищем минимум, максимум и нужный столбец
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if (matrix[j][i] >= max_value) {
@@ -59,6 +56,7 @@ Matrix MatrixWithModifiedCols(Matrix matrix, int32_t n) {
             }
         }
     }
+
     // Производим замену столбцов
     if (min_value != max_value) {
         bool has_min_value = false;
@@ -77,32 +75,36 @@ Matrix MatrixWithModifiedCols(Matrix matrix, int32_t n) {
             }
         }
     }
+
     return matrix;
 }
 
 int Task() {
     int32_t n = 0;
-    // Вводим сюда название файла
-    const char* filename = "matrix.txt";
+
+    // Вводим сюда путь к файлу
+    const char* filename = "../tasks/matrix/tasks/30/matrix.txt";
     Matrix matrix = InitializeMatrixFromFile(filename, &n);
-    if (matrix != 0) {
-        // Вывод исходной матрицы
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                printf("%d ", matrix[i][j]);
-            }
-            printf("\n");
+
+    // Вывод исходной матрицы
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
-        matrix = MatrixWithModifiedCols(matrix, n);
-        // Вывод итоговой матрицы
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                printf("%d ", matrix[i][j]);
-            }
-            printf("\n");
-        }
-        FreeMatrix(matrix, n);
     }
+    printf("\n");
+
+    // Преобразуем матрицу
+    matrix = MatrixWithModifiedCols(matrix, n);
+
+    // Вывод итоговой матрицы и освобождение памяти
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    FreeMatrix(matrix, n);
     return 0;
 }
