0004342df035f0fb63b8a980b8f6f94179c0e8a0
DmitryNai <144270336+DmitryNai@users.noreply.github.com>
2023-11-10 18:21:49 +0300

    Update solution.h

--- tasks/linerization/tasks/22/solution.h
+++ tasks/linerization/tasks/22/solution.h
@@ -2,20 +2,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef int32_t** Matrix;
 
-Matrix CreateMatrix(int32_t lenght, int32_t hight) {
-    Matrix matrix = (Matrix)malloc(lenght * sizeof(int32_t*));
-    for (int32_t i = 0; i < lenght; ++i) {
-        matrix[i] = (int32_t*)malloc(hight * sizeof(int32_t));
+Matrix CreateMatrix(int32_t height, int32_t width) {
+    Matrix matrix = (Matrix)malloc(height * sizeof(int32_t*));
+    for (int32_t i = 0; i < height; ++i) {
+        matrix[i] = (int32_t*)malloc(width * sizeof(int32_t));
     }
 
     return matrix;
 }
 
-void MatrixFree(Matrix matrix, int32_t hight) {
-    for (int32_t i = 0; i < hight; ++i) {
+void MatrixFree(Matrix matrix, int32_t height) {
+    for (int32_t i = 0; i < height; ++i) {
         free(matrix[i]);
     }
     free(matrix);
@@ -40,64 +41,69 @@ void MatrixInitiate(Matrix matrix) {
     matrix[3][3] = 7;
 }
 
-int32_t* LinealMatrix(int32_t** matrix, int32_t lenght, int32_t hight) {
-    int32_t* lineal_matrix = (int32_t*)malloc(lenght * hight * sizeof(int32_t));
+int32_t* LinealMatrix(int32_t** matrix, int32_t height, int32_t width) {
+    int32_t* lineal_matrix = (int32_t*)malloc(height * width * sizeof(int32_t));
     int32_t top = 0;
-    int32_t bottom = lenght - 1;
+    int32_t bottom = height - 1;
     int32_t left = 0;
-    int32_t right = hight - 1;
+    int32_t right = width - 1;
     int32_t index_of_lineal = 0;
     int32_t direction = 0;
-
+  
     while (top <= bottom && left <= right) {
         if (direction == 0) {
-            for (int32_t i = left; i <= right; i++) {
-                lineal_matrix[index_of_lineal] = matrix[i][top];
-                ++index_of_lineal;
-            }
-            top++;
-        } else if (direction == 1) {
             for (int32_t i = top; i <= bottom; i++) {
-                lineal_matrix[index_of_lineal] = matrix[right][i];
-                ++index_of_lineal;
-            }
-            right--;
-        } else if (direction == 2) {
-            for (int32_t i = right; i >= left; i--) {
-                lineal_matrix[index_of_lineal] = matrix[i][bottom];
-                ++index_of_lineal;
-            }
-            bottom--;
-        } else if (direction == 3) {
-            for (int32_t i = bottom; i >= top; i--) {
-                lineal_matrix[index_of_lineal] = matrix[left][i];
+                lineal_matrix[index_of_lineal] = matrix[i][left];
                 ++index_of_lineal;
             }
             left++;
         }
-        direction = (direction + 1) % 4;
+        else if (direction == 1) {
+            for (int32_t i = left; i <= right; i++) {
+                lineal_matrix[index_of_lineal] = matrix[bottom][i];
+                ++index_of_lineal;
+            }
+            bottom--;
+        }
+        else if (direction == 2) {
+            for (int32_t i = bottom; i >= top; i--) {
+                lineal_matrix[index_of_lineal] = matrix[i][right];
+                ++index_of_lineal;
+            }
+            right--;
+        }
+        else if (direction == 3) {
+            for (int32_t i = right; i >= left; i--) {
+                lineal_matrix[index_of_lineal] = matrix[top][i];
+                ++index_of_lineal;
+            }
+            top++;
+        }
+    direction = (direction + 1) % 4;
     }
     return lineal_matrix;
 }
 
 int Task() {
-    int32_t lenght = 4;
-    int32_t hight = 4;
-    Matrix matrix = CreateMatrix(lenght, hight);
+    int32_t height = 4;
+    int32_t width = 3;
+    Matrix matrix = CreateMatrix(height, width);
     MatrixInitiate(matrix);
-    int32_t* lineal_matrix = LinealMatrix(matrix, lenght, hight);
 
-    for (int32_t i = 0; i < hight; ++i) {
-        for (int32_t j = 0; j < lenght; ++j) {
+    for (int32_t i = 0; i < height; ++i) {
+        for (int32_t j = 0; j < width; ++j) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
-    for (int32_t i = 0; i < hight * lenght; ++i) {
+
+    int32_t* lineal_matrix = LinealMatrix(matrix, height, width);
+    
+    for (int32_t i = 0; i < width * height; ++i) {
         printf("%d ", lineal_matrix[i]);
     }
-
+    
     free(lineal_matrix);
-    MatrixFree(matrix, lenght);
+    MatrixFree(matrix, height);
     return 0;
 }
