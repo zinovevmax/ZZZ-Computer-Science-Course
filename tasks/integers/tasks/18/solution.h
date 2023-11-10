cc871c26447af38264a0a38d497b47ea95f0bbc0
mashulyag <ya.merri2005@yandex.ru>
2023-11-10 15:57:34 +0300

    lalala

--- tasks/integers/tasks/18/solution.h
+++ tasks/integers/tasks/18/solution.h
@@ -6,34 +6,35 @@
 
 const int64_t BASE = 10;
 const int64_t EXAMPLE = 1234;
-const int64_t MIN_OR_MAX_INT_WITHOUT_SIGN = 9223372036854775808;
 
 int Task() {
     // write your solution here
-    int64_t number_with_sign = EXAMPLE;
-    int64_t sign = 0;
-    if (number_with_sign < 0) {
-        sign = -1;
+    if (EXAMPLE == INT64_MIN) {
+        printf("-9023372036854775828");
     } else {
-        sign = 1;
-    }
-    if (EXAMPLE == MIN_OR_MAX_INT_WITHOUT_SIGN) {
-        int64_t new_int64_with_permutations = 9023372036854775828;
-        printf("%ld\n", new_int64_with_permutations * sign);
-    } else {
+
+        int64_t number_with_sign = EXAMPLE;
+        int64_t sign = 0;
+        if (number_with_sign < 0) {
+            sign = -1;
+        } else {
+            sign = 1;
+        }
+
         int64_t number = abs(EXAMPLE);
         int64_t copy_number = abs(EXAMPLE);
-        int64_t second = 0;
-        int64_t penultimate = (number / BASE) % BASE;
-        int64_t position_of_second = 1;
+        int64_t second_digit = 0;
+        int64_t penultimate_digit = (number / BASE) % BASE;
+        int64_t position_of_second_digit = 1;
         while (copy_number > BASE) {
-            second = copy_number % BASE;
+            second_digit = copy_number % BASE;
             copy_number = copy_number / BASE;
-            position_of_second = position_of_second * BASE;
+            position_of_second_digit = position_of_second_digit * BASE;
         }
-        position_of_second = position_of_second / BASE;
-        int64_t new_number = (number - second * position_of_second - penultimate * BASE +
-                              penultimate * position_of_second + second * BASE) *
+        position_of_second_digit = position_of_second_digit / BASE;
+
+        int64_t new_number = (number - second_digit * position_of_second_digit - penultimate_digit * BASE +
+                              penultimate_digit * position_of_second_digit + second_digit * BASE) *
                              sign;
         printf("%ld\n", new_number);
     }
