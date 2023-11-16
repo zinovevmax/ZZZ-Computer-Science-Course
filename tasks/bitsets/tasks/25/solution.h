493798f31db5be0597e49c556aaeb8b3555b8bd1
xcus33me <harvieorigin@yandex.ru>
2023-11-16 12:10:42 +0300

    tёstin

--- tasks/bitsets/tasks/25/solution.h
+++ tasks/bitsets/tasks/25/solution.h
@@ -10,31 +10,44 @@ typedef uint32_t Bitset;
 // Sibilant consonants:
 // sh, ch, zh, th
 
-Bitset sibilantConsonants = 0;
+Bitset sibilant_consonants = 0;
 
 bool CheckForSibilantConsonants(char c) {
-    c = tolower(c);
-    if ((c == 's') || (c == 'c') || (c == 'h') || (c == 't') || (c == 'z')) {
-        if (sibilantConsonants == 0) {
-            if (c != 'h')
-                sibilantConsonants |= (1u << (c - 'a'));
-            return false;
-        } else {
-            if (c == 'h')
-                return true;
-        }
+    
+    if (isalpha(c)) {
+        c = tolower(c);
+    }
+    if (((c == 's') || (c == 'c') || (c == 't') || (c == 'z')) && (sibilant_consonants == 0)) {
+        sibilant_consonants |= (1u << (c - 'a'));
+        return false;
+    } else if ((c == 'h') && (sibilant_consonants != 0)) {
+        sibilant_consonants |= (1u << (c - 'a'));
+        return true;
     } else {
-        sibilantConsonants = 0;
+        sibilant_consonants = 0;
         return false;
     }
 }
 
+void PrintSibilantConsonant() {
+    char symbol = 0;
+    for (size_t i = 0; i < 26; ++i) {
+        if (sibilant_consonants & (1u << i)) {
+            symbol = 'a' + i;
+            if (symbol != 'h') {
+                printf("%s", (char)symbol);
+            }
+        }
+    }
+    printf("%s", 'h');
+}
+
 int Task() {
-    char input_letter;
+    char input_letter = 0;
     while ((input_letter = (char)getchar()) != EOF) {
-        bool answer = CheckForSibilantConsonants(input_letter);
-        if (answer) {
-            printf("A word consisting of at least one sibilant was detected!");
+        if (CheckForSibilantConsonants(input_letter)) {
+            printf("A word consisting of at least one sibilant was detected: ");
+            PrintSibilantConsonant();
             break;
         }
     }
