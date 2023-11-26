1f6db20d71e8349140ff434339d44c1b84d485e7
German2104 <139492070+German2104@users.noreply.github.com>
2023-11-26 11:45:11 +0300

    BITTSETS_16_PERMYAKOV.h

--- tasks/bitsets/tasks/16/solution.h
+++ tasks/bitsets/tasks/16/solution.h
@@ -5,7 +5,35 @@
 
 typedef uint32_t Bitset;
 
+unsigned int char_to_set(char c) {
+    c = tolower(c);
+    if (c < 'a' || c > 'z' || c == 'a' || c == 'u' || c == 'o' || c == 'e' || c == 'i') {
+        return 0;
+    } else {
+        return 1u << (c - 'a');
+    }
+}
+
 int Task() {
     // write your solution here
+    char c;
+    unsigned int letters_set_1 = 0;
+    unsigned int letters_set_2 = 0;
+    bool answer = false;
+    while ((c = getchar()) != EOF) {
+        letters_set_2 = letters_set_2 | char_to_set(c);
+        if (c == ' ' || c == '\n') {
+            if (letters_set_2 == letters_set_1) {
+                answer = true;
+            }
+            letters_set_1 = letters_set_2;
+            letters_set_2 = 0;
+        }
+    }
+    if (answer == true) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
     return 0;
 }
