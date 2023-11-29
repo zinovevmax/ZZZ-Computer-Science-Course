323701fbdfa1f6bced0d4d3edf62f8c7b7356af1
Nikita Krushinin <144558570+qwizy@users.noreply.github.com>
2023-11-29 19:55:42 +0300

    INT_5_KRUSHININ_NIKITA

--- tasks/integers/tasks/5/solution.h
+++ tasks/integers/tasks/5/solution.h
@@ -3,6 +3,10 @@
 #include <stdint.h>
 #include <stdio.h>
 
+const uint64_t NUMBER_10 = 10;
+const uint64_t NUMBER_100 = 100;
+const uint64_t NUMBER_1000 = 1000;
+
 int Task() {
     int flag = 0;
     uint64_t number = 358325775252;
@@ -10,17 +14,17 @@ int Task() {
     uint64_t second_number = 0;
     uint64_t third_number = 0;  
 
-    while (number >= 100) {
-        first_number = number % 10;
-        second_number = number % 100 / 10;
-        third_number = number % 1000 / 100;
+    while (number >= NUMBER_100) {
+        first_number = number % NUMBER_10;
+        second_number = number % NUMBER_100 / NUMBER_10;
+        third_number = number % NUMBER_1000 / NUMBER_100;
 
         if (first_number == second_number + third_number) {
             printf("%d\n", first_number);
             flag = 1;
         }
 
-        number /= 10;
+        number /= NUMBER_10;
     }
 
     if (flag == 0) {
