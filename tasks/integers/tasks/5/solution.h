442b42db214b1ecd1680f10bd039964b958e2064
Nikita Krushinin <144558570+qwizy@users.noreply.github.com>
2023-11-30 17:59:53 +0300

    INT_5_KRUSHININ_NIKITA

--- tasks/integers/tasks/5/solution.h
+++ tasks/integers/tasks/5/solution.h
@@ -4,22 +4,22 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-uint64_t FirstNumber (uint64_t number) {
+uint64_t FirstNumber(uint64_t number) {
     return number % 10;
 }
 
-uint64_t SecondNumber (uint64_t number) {
+uint64_t SecondNumber(uint64_t number) {
     return number % 100 / 10;
 }
 
-uint64_t ThirdNumber (uint64_t number) {
+uint64_t ThirdNumber(uint64_t number) {
     return number % 1000 / 100;
 }
 
 int Task() {
     bool flag = false;
     uint64_t number = 358325775252;
-    
+
     while (number >= 100) {
         if (FirstNumber(number) == SecondNumber(number) + ThirdNumber(number)) {
             printf("%lu\n", FirstNumber(number));
