ab49ba424dc9666d6539b5950b4ad067e148bd93
sergey <usov.serzh.04@mail.ru>
2023-12-01 11:35:58 -0500

    New

--- tasks/taylor/tasks/17/solution.h
+++ tasks/taylor/tasks/17/solution.h
@@ -6,44 +6,56 @@
 #include <stdint.h>
 #include <stdio.h>
 
-double NatyralLog(double x) {
+long double Func(long double x) {
     return 0.5 * log(x);
 }
 
-double Degree(double exp, uint8_t n) {
-    double copy_exp = 1.0;
+long double Degree(long double exp, uint8_t n) {
+    long double res_exp = 1.0;
     while (n > 0) {
-        copy_exp *= exp;
+        res_exp *= exp;
         --n;
     }
-    return copy_exp;
+    return res_exp;
 }
-double ResultInPoint(double x, uint16_t iterations, uint8_t precision) {
-    double result = 0.0;
+
+long double CalcPerIter(long double x, int iter) {
+    return (long double)1 / (2 * iter + 1) * Degree((x - 1.0) / (x + 1.0), 2 * iter + 1);
+}
+
+bool PositiveCalc(long double x, long double tailor_result, uint8_t precision) {
+    return fabsl(tailor_result - Func(x)) < DBL_EPSILON * precision;
+}
+
+long double TailorResult(long double x, uint16_t iterations, uint8_t precision) {
+    long double result = 0.0;
     for (int i = 0; i < iterations; ++i) {
-        result += (double)1 / (2 * i + 1) * Degree((x - 1.0) / (x + 1.0), 2 * i + 1);
-        if (fabs(result - NatyralLog(x)) < DBL_EPSILON * precision) {
+        result += CalcPerIter(x, i);
+        if (PositiveCalc(x, result, precision)) {
             break;
         }
     }
     return result;
 }
-void PrintString(double x, double tailor_result, double func_result) {
+
+void PrintString(long double x, long double tailor_result, long double func_result) {
     static int iter = 0;
-    printf("iter = %d  x = %.2lf Tailor = %.10lf Func = %.10lf\n", iter, x, tailor_result, func_result);
+    printf("iter = %d  x = %.3Lf Tailor = %.12Lf Func = %.12Lf\n", iter, x, tailor_result, func_result);
     ++iter;
 }
 
-int Task() {
+int main() {
+    const uint8_t num_bits_fract = 52;
+    uint8_t precision = 40;
+    // My precision output == num_bits_fract - precision
     uint8_t n = 10;
     uint16_t iterations_for_tailor = 100;
-    uint8_t precision = 10;
-    const double a = 0.2;
-    const double b = 0.7;
-    double step = (b - a) / n;
-    double x = a;
+    const long double a = 0.2;
+    const long double b = 0.7;
+    long double step = (b - a) / n;
+    long double x = a;
     for (int i = 0; i <= n; ++i) {
-        PrintString(x, ResultInPoint(x, iterations_for_tailor, precision), NatyralLog(x));
+        PrintString(x, TailorResult(x, iterations_for_tailor, precision), Func(x));
         x += step;
     }
     return 0;
