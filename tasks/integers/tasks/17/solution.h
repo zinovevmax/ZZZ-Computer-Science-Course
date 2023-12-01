accbb1376e8a7e83c0259f17965cb840ad7f6f78
GorbanArtemiy <artygor5@yandex.ru>
2023-12-02 00:49:46 +0300

    var17

--- tasks/integers/tasks/17/solution.h
+++ tasks/integers/tasks/17/solution.h
@@ -4,6 +4,16 @@
 #include <stdio.h>
 
 int Task() {
-    // write your solution here
+    int number = 0;
+    scanf("%d", &number);
+    int big_number = 10;
+    int length_counter = 10;
+    while (big_number > 9) {
+        big_number = number / length_counter;
+        length_counter *= 10;
+    }
+    int small_number = number % 10;
+    int new_number = small_number * length_counter + ((number % length_counter) / 10) * 10 + big_number;
+    printf("%p", (void*)&new_number);
     return 0;
 }
