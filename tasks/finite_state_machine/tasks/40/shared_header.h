662c945cb6032d52470cb75522273d142469ba9f
DmitryNai <144270336+DmitryNai@users.noreply.github.com>
2023-12-02 23:19:35 +0300

    Update shared_header.h

--- tasks/finite_state_machine/tasks/40/shared_header.h
+++ tasks/finite_state_machine/tasks/40/shared_header.h
@@ -4,17 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum { NUMCHECK = 0, REVERSE, CONVERT, STATECOUNT, END } StateName;
+typedef enum { START = 0, NUMCHECK, VALUEFORMING, CONVERT, STATECOUNT, END } StateName;
 
 typedef struct State State;
 
 typedef struct {
     State* pipeline;
     StateName cur_state;
-    FILE* file;
     int reversed_number;
     int digit;
     int octal_number;
+    char ch;
 } StateMachine;
 
 typedef struct State {
