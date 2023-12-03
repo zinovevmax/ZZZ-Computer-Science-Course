307b24d8c5467697dda3b71d1379d90b9bb9ccdf
JDorof <demadorofeevicok977@gmail.com>
2023-12-03 16:05:07 +0300

    fixing code after review

--- tasks/bitsets/tasks/2/solution.h
+++ tasks/bitsets/tasks/2/solution.h
@@ -5,43 +5,46 @@
 #include <stdio.h>
 
 typedef uint32_t Bitset;
-const uint32_t CONSONANTS = 66043630u;  // number of Bitset consonants
+Bitset CONSONANTS = 0u;  // number of Bitset consonants
 
 // Getting Bitset of consonants
-// Bitset ConsonantsAlphabet() {
-//     char text[] = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x',
-//     'y', 'z'}; uint32_t Set = 0; for (int i = 0; i < strlen(text); ++i) { Set = Set | (1u << (text[i] - 'a')); return
-//     Set
-//     }
-// }
+Bitset ConsonantsAlphabet() {
+    char text[] = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
+    uint32_t Set = 0;
+    for (int i = 0; i < strlen(text); ++i) {
+        Set = Set | (1u << (text[i] - 'a'));
+    }
+    return Set
+}
 
-bool CheckIfConsonant(char sign) {
+bool IsConsonant(char sign) {
     return (CONSONANTS == (CONSONANTS | (1u << (sign - 'a'))));
 }
 
-bool CheckIfBeenBefore(char sign, Bitset consonants_before) {
+bool IsBeenBefore(char sign, Bitset consonants_before) {
     return (consonants_before >= (consonants_before ^ (1u << (sign - 'a'))));
 }
 
 int Task() {
-    Bitset consonants_before = 0;
     bool repeated_consonant = false;
-    char sign = 0;
     uint32_t space_counter = 0;
+    Bitset consonants_before = 0;
+    char sign = 0;
+    CONSONANTS = ConsonantsAlphabet();
     while ((sign = (char)getchar()) != EOF) {
         if ((sign == ' ') || (sign == '\n')) {
             if ((repeated_consonant == false) && (space_counter == 0)) {
                 printf("there is at least one word with non-repeating consonants");
                 return 0;
             }
-            space_counter++;
+            ++space_counter;
             consonants_before = 0;
             repeated_consonant = false;
             continue;
         }
         space_counter = 0;
-        if (CheckIfConsonant(sign)) {
-            if (CheckIfBeenBefore(sign, consonants_before) == false) {
+        if (IsConsonant(sign)) {
+            if (IsBeenBefore(sign, consonants_before) == false) {
                 consonants_before = (consonants_before ^ (1u << (sign - 'a')));
             } else {
                 repeated_consonant = true;
