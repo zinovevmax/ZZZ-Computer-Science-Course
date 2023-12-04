987018eb2c8239fa873dcf9f014bc975433a93e6
sergey <usov.serzh.04@mail.ru>
2023-12-04 14:20:13 -0500

    v.1.3

--- tasks/taylor/tasks/17/solution.h
+++ tasks/taylor/tasks/17/solution.h
@@ -19,41 +19,46 @@ long double Degree(long double exp, uint8_t n) {
     return res_exp;
 }
 
-long double CalcPerIter(long double x, int iter) {
+long double TaylorSeriesTerm(long double x, int iter) {
     return (long double)1 / (2 * iter + 1) * Degree((x - 1.0) / (x + 1.0), 2 * iter + 1);
 }
 
-bool PositiveCalc(long double x, long double tailor_result, uint8_t precision) {
+bool IsBreakCalc(long double x, long double tailor_result, uint8_t precision) {
     return fabsl(tailor_result - Func(x)) < DBL_EPSILON * precision;
 }
 
-long double TailorResult(long double x, uint16_t iterations, uint8_t precision) {
+long double TailorResult(long double x, uint16_t iterations, uint8_t precision, uint8_t* num_taylor_it) {
+    *num_taylor_it = 0;
     long double result = 0.0;
     for (int i = 0; i < iterations; ++i) {
-        result += CalcPerIter(x, i);
-        if (PositiveCalc(x, result, precision)) {
+        ++(*num_taylor_it);
+        result += TaylorSeriesTerm(x, i);
+        if (IsBreakCalc(x, result, precision)) {
             break;
         }
     }
     return result;
 }
 
-void PrintTable(long double x, long double tailor_result, long double func_result) {
+void PrintTable(long double x, long double tailor_result, long double func_result, uint8_t num_taylor_it) {
     static int iter = 0;
-    printf("iter = %d  x = %.3Lf Tailor = %.12Lf Func = %.12Lf\n", iter, x, tailor_result, func_result);
+    printf("iter = %d  x = %.3Lf Tailor = %.12Lf Func = %.12Lf\n NumTaylorIt - %u", iter, x, tailor_result, func_result, num_taylor_it);
     ++iter;
 }
 
-int Task() {
+int main() {
     uint8_t precision = 40;
+    uint8_t num_taylor_it = 0;
     uint8_t n = 10;
     uint16_t iterations_for_tailor = 100;
     const long double a = 0.2;
     const long double b = 0.7;
     long double step = (b - a) / n;
     long double x = a;
+    long double taylor_result = 0.0;
     for (int i = 0; i <= n; ++i) {
-        PrintTable(x, TailorResult(x, iterations_for_tailor, precision), Func(x));
+        taylor_result = TailorResult(x, iterations_for_tailor, precision, &num_taylor_it);
+        PrintTable(x, Func(x), taylor_result, num_taylor_it);
         x += step;
     }
     return 0;
