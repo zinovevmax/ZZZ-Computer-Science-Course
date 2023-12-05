cf563a173eb19d35bbae89fa4e45a9fcba045277
Мария <144603221+mashulyag@users.noreply.github.com>
2023-12-05 21:46:27 +0300

    improve solution

--- tasks/taylor/tasks/6/solution.h
+++ tasks/taylor/tasks/6/solution.h
@@ -9,39 +9,49 @@ long double Func(long double x) {
     return (exp(x) - exp(-x)) / 2;
 }
 
+// Вывод заголовка таблицы
+void TableTitleOutput() {
+    printf("-----------------------------------------------------------------\n");
+    printf("  Taylor series values table for f(x) = (exp(x) - exp(-x)) / 2  |\n");
+    printf("-----------------------------------------------------------------\n");
+    printf("|     X     |       Taylor      |      Function     | Iteration |\n");
+    printf("-----------------------------------------------------------------\n");
+}
+
+// Вывод строки таблицы
+void LineOutput(long double x, long double row_of_taylor, int count_iterations) {
+    printf("| %.7Lf | %.15Lf | %.15Lf | %.9d |\n", x, row_of_taylor, Func(x), count_iterations);
+    printf("-----------------------------------------------------------------\n");
+}
+
 int Task() {
+    TableTitleOutput();
     double a = 0.0;
     double b = 1.0;
     int32_t n = 6;
-    int32_t k = 100;
+    int32_t k = 50;
     int32_t count_iterations = 0;
     long double factorial = 0;
     long double row_of_taylor = 0;
-
-    if (n <= 1 || k <= 1 || k > 100) {
-        printf("Incorrect values have been entered.");
-    } else {
-        // printing table
-        printf("-----------------------------------------------------------------\n");
-        printf("|     X     |       Taylor      |      Function     | Iteration |\n");
-        printf("-----------------------------------------------------------------\n");
-        long double step = (b - a) / (double)(n - 1);
-        long double x = a;
-        for (int i = 0; i < n; ++i) {
-            factorial = 1.0;
-            count_iterations = 0;
-            row_of_taylor = 0;
-            for (int j = 1; j <= k; ++j) {
-                factorial = factorial * j;
-                if (j % 2 != 0 && j <= (2 * n - 1)) {
-                    row_of_taylor = row_of_taylor + pow(x, j) / factorial;
-                    count_iterations = count_iterations + 1;
-                }
+    long double step = (b - a) / (double)(n - 1);
+    long double precision = LDBL_EPSILON * k;
+    long double x = a;
+    for (int i = 0; i < n; ++i) {
+        count_iterations = 0;
+        row_of_taylor = 0.0;
+        long double extension = LDBL_MAX;
+        factorial = 1.0;
+        while (fabsl(extension) >= precision) {
+            count_iterations = count_iterations + 1;
+            factorial = factorial * count_iterations;
+            if (count_iterations % 2 != 0) {
+                extension = pow(x, count_iterations) / factorial;
+                row_of_taylor = row_of_taylor + extension; 
             }
-            printf("| %.7Lf | %.15Lf | %.15Lf | %.9d |\n", x, row_of_taylor, Func(x), count_iterations);
-            x = x + step;
-            printf("-----------------------------------------------------------------\n");
         }
+        LineOutput(x, row_of_taylor, count_iterations);
+        x = x + step;
     }
+    printf("* machine epsilon (long double) = %.10Le\n", LDBL_EPSILON);
     return 0;
 }
