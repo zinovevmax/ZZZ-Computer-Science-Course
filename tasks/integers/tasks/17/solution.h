c7349b1c3b3aaf068f7587d757e8a579a7a94de7
GorbanArtemiy <artygor5@yandex.ru>
2023-12-08 17:42:56 +0300

    var 17 fix

--- tasks/integers/tasks/17/solution.h
+++ tasks/integers/tasks/17/solution.h
@@ -4,16 +4,16 @@
 #include <stdio.h>
 
 int Task() {
-    int number = 0;
+    uint32_t number = 0;
     scanf("%d", &number);
-    int big_number = 10;
-    int length_counter = 10;
-    while (big_number > 9) {
-        big_number = number / length_counter;
+    uint32_t first_digit = 10;
+    uint32_t length_counter = 10;
+    while (first_digit > 9) {
+        first_digit = number / length_counter;
         length_counter *= 10;
     }
-    int small_number = number % 10;
-    int new_number = small_number * length_counter + ((number % length_counter) / 10) * 10 + big_number;
-    printf("%p", (void*)&new_number);
+    uint32_t last_digit = number % 10;
+    uint32_t new_number = last_digit * length_counter + ((number % length_counter) / 10) * 10 + first_digit;
+    printf("%d", new_number);
     return 0;
 }
