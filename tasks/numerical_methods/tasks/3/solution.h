6646e03cca5b011fac8b26c737a3c9d390820581
mashulyag <ya.merri2005@yandex.ru>
2023-12-08 22:45:06 +0300

    lalala

--- tasks/numerical_methods/tasks/3/solution.h
+++ tasks/numerical_methods/tasks/3/solution.h
@@ -1,10 +1,64 @@
 #pragma once
 
 #include <float.h>
+#include <math.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
+#define db double
+
+const double APPROXIMATION_VALUE_1 = 1.1474;
+const double APPROXIMATION_VALUE_2 = 2.0692;
+
+double Func1(double x) {
+    return 1 - x + sin(x) - log(1 + x);
+}
+
+double Func2(double x) {
+    return 3 * x - 14 + exp(x) - exp(-x);
+}
+
+double MethodOfIterative(double x0, double x1, double (*func)(double)) {
+    double x = (x0 + x1) / 2.0;
+    double prev_x = x0;
+    while (fabsl((func(x) - x) / (1 - ((func(x) - x) / (x - prev_x)))) >= DBL_EPSILON) {
+        prev_x = x;
+        x = func(x) + x;
+    }
+    return func(x) + x;
+}
+
+double CalculatingOfDerivatie(double x) {
+    return 3 + exp(x) + exp(x);
+}
+
+double MethodOfNewton(double x0, double x1, double (*func)(double)) {
+    double xn0 = (x0 + x1) / 2.0;
+    double xn1 = xn0 - func(xn0) / CalculatingOfDerivatie(xn0);
+    while (fabs((xn1 - xn0) / (1.0 - ((xn1 - xn0) / (xn1 - xn0)))) > DBL_EPSILON) {
+        xn0 = xn1;
+        xn1 = xn0 - func(xn0) / CalculatingOfDerivatie(xn0);
+    }
+    return xn1;
+}
+
+void OutputAnswer(db x0, db x1, db (*func)(db), db (*method)(db, db, db (*)(db)), uint8_t precision, const db ex_val) {
+    printf("-Отрезок: [%.2lf, %.2lf]\n", x0, x1);
+    printf("-Найденный корень: %.*f\n", precision, method(x0, x1, func));
+    printf("-Значение для сравнения: %g\n", ex_val);
+    printf("-------------------------------\n");
+}
+
 int Task() {
-    // write your solution here
+    double start_section_1 = 1.0;
+    double start_section_2 = 1.0;
+    double end_section_1 = 1.5;
+    double end_section_2 = 3.0;
+    printf("-------------------------------\n");
+    printf("Method: Iterations\n");
+    OutputAnswer(start_section_1, end_section_1, Func1, MethodOfIterative, 10, APPROXIMATION_VALUE_1);
+    printf("Method: Newton\n");
+    OutputAnswer(start_section_2, end_section_2, Func2, MethodOfNewton, 10, APPROXIMATION_VALUE_2);
     return 0;
 }
