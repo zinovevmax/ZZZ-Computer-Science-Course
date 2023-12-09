d8e68d4557741794598839a919e1a9db2fd35fdc
mnlyuzina <mnlyuzina@yandex.ru>
2023-12-09 17:06:52 +0300

    int29

--- tasks/integers/tasks/29/solution.h
+++ tasks/integers/tasks/29/solution.h
@@ -3,36 +3,28 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const uint8_t TEN = 10;
-const uint8_t EIGHT = 8;
-
-int SquareOfTen(uint8_t TEN, uint32_t cycle_counter) {
-    uint32_t result_of_function = 1;
+int SquareOfTen(int cycle_counter) {
+    int result_of_function = 1;
     for (size_t degree_counter = 1; degree_counter <= cycle_counter; ++degree_counter) {
-        result_of_function = result_of_function * TEN;
+        result_of_function = result_of_function * 10;
     }
     return result_of_function;
 }
 
-int Mod(uint32_t input_number, uint8_t EIGHT) {
-    return input_number % EIGHT;
-}
-
-int ConvertFromDecimalToOctal(uint32_t input_number) {
-    uint32_t output_number = 0;
-    uint32_t cycle_counter = 0;
+int ConvertFromDecimalToOctal(int input_number) {
+    int output_number = 0;
+    int cycle_counter = 0;
     while (input_number > 0) {
-        output_number += Mod(input_number, EIGHT) * SquareOfTen(TEN, cycle_counter);
+        output_number += (input_number % 8) * SquareOfTen(cycle_counter);
         ++cycle_counter;
-        input_number /= EIGHT;
+        input_number /= 8;
     }
     return output_number;
 }
+
 int Task() {
-    uint32_t input_number = 0;
-    printf("Enter a number:\n");
-    scanf("%lu", &input_number);
-    int result = ConvertFromDecimalToOctal(input_number);
-    printf("The result: %d\n", result);
+    uint64_t input_number = 3456748945;  // input number here
+    uint64_t result = ConvertFromDecimalToOctal(input_number);
+    printf("The result: %u\n", result);
     return 0;
 }
