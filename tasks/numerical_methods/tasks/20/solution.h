9a4e2580c656c4aabe54c85062ec5c79f38c6585
JDorof <demadorofeevicok977@gmail.com>
2023-12-09 12:44:44 +0300

    putting functions in arguments

--- tasks/numerical_methods/tasks/20/solution.h
+++ tasks/numerical_methods/tasks/20/solution.h
@@ -13,14 +13,14 @@ double DerivativeFuncNewton(double x) {
     return 0.2 * x - 1.0;
 }
 
-double Newton() {
+double NewtonsMethod(double (*func)(double), double (*derivative_func)(double)) {
     double a = 1.0;
     double b = 2.0;
     double x_previous = a;
     double x = b;
     while (abs(x - x_previous) > DBL_EPSILON) {
-        x = x_previous - (FuncNewton(x_previous) / DerivativeFuncNewton(x_previous));
-        x_previous = x - (FuncNewton(x) / DerivativeFuncNewton(x));
+        x = x_previous - (func(x_previous) / derivative_func(x_previous));
+        x_previous = x - (func(x) / derivative_func(x));
     }
     return x;
 }
@@ -29,13 +29,13 @@ double FuncDichotomy(double x) {
     return tan(x) - (pow(tan(x), 3.0) / 3.0) + (pow(tan(x), 5.0) / 5.0) - (1.0 / 3.0);
 }
 
-double Dichotomy() {
+double DichotomyMethod(double (*func)(double)) {
     double a = 0.0;
     double b = 0.8;
     double middle = 0.0;
     while (abs(a - b) > DBL_EPSILON) {
         middle = (a + b) / 2.0;
-        if (FuncDichotomy(a) * FuncDichotomy(middle) < 0.0) {
+        if (func(a) * func(middle) < 0.0) {
             b = middle;
         } else {
             a = middle;
@@ -45,8 +45,8 @@ double Dichotomy() {
 }
 
 int Task() {
-    double newton_result = Newton();
-    double dichotomy_result = Dichotomy();
+    double newton_result = NewtonsMethod(*FuncNewton, *DerivativeFuncNewton);
+    double dichotomy_result = DichotomyMethod(*FuncDichotomy);
     printf("Newton's Method:      %lf\n", newton_result);
     printf("Method of Dichotomy:  %lf\n", dichotomy_result);
     return 0;
