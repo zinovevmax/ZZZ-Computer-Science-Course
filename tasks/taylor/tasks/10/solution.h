b9a07890afd8ce4920da02850ed3063fec01a6b9
JDorof <demadorofeevicok977@gmail.com>
2023-12-12 22:45:09 +0300

    fixing after review

--- tasks/taylor/tasks/10/solution.h
+++ tasks/taylor/tasks/10/solution.h
@@ -9,24 +9,29 @@ double Func(double x) {
     return sin(x) * sin(x);
 }
 
-double Indent(double a, double b, int32_t n) {
+double Step(double a, double b, int32_t n) {
     return (b - a) / (double)n;
 }
 
+double CalculatingTaylorTerm(double x, double prev_y, int32_t iteration) {
+    return prev_y * (double)((-4) * pow(x, 2.0)) / (double)((iteration * 2 - 1) * (iteration * 2));
+}
+
 void Taylor(double x, double y_func) {
+    double precision = 1.0;
     double y = 0;
-    double previous_y = pow(x, 2.0);
-    double new_y = 0;
+    double prev_y = pow(x, 2.0);
+    double next_y = 0;
     int32_t iterations = 1;
-    y += previous_y;
+    y += prev_y;
     for (int32_t iteration = 2u; iteration < 100; ++iteration) {
-        if (abs(y_func - y) < DBL_EPSILON) {
+        if (abs(y_func - y) < DBL_EPSILON * precision) {
             break;
         }
         ++iterations;
-        new_y = previous_y * (double)((-4) * pow(x, 2.0)) / (double)((iteration * 2 - 1) * (iteration * 2));
-        y += new_y;
-        previous_y = new_y;
+        next_y = CalculatingTaylorTerm(x, prev_y, iteration);
+        y += next_y;
+        prev_y = next_y;
     }
     printf("%.25lf     |%i\n", y, iterations);
 }
@@ -36,7 +41,7 @@ int Task() {
     double a = 0.0;
     double b = 1.0;
     double x = a;
-    double indent = Indent(a, b, n);
+    double indent = Step(a, b, n);
     printf("x                |func                            |taylor                          |iterations\n");
     for (int32_t k = 0; k < n + 1; ++k) {
         printf("%.10lf     |%.25lf     |", x, Func(x));
