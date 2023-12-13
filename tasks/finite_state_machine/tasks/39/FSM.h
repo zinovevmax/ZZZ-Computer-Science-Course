cff0299569db765eaa81f10128bbf71bdff572e8
Matvey26 <matvey.ivchenko@yandex.ru>
2023-12-13 19:11:17 +0300

    Update regexpr

--- tasks/finite_state_machine/tasks/39/FSM.h
+++ tasks/finite_state_machine/tasks/39/FSM.h
@@ -5,7 +5,7 @@
 #include "fsm_realization.h"
 
 // решение в регвырах:
-// 0[xX][8-9A-Fa-f][0-9A-Fa-f]{3} последнее число в {} это просто HEX_LENGTH - 1 (see shared_header.h)
+// 0[xX][a-fA-F0-9]{6}[8-9a-fA-F][a-fA-F0-9] число в {} это 2 * (HEX_LENGTH - 1) (see shared_header.h)
 
 int Task() {
     StateMachine* sm = CreateStateMachine();
