8d47cdff85c31478b401ffadfb31924996c85938
rezect <ilyapr1568@yandex.ru>
2023-12-13 18:54:58 +0200

    Update solution.h

--- tasks/taylor/tasks/13/solution.h
+++ tasks/taylor/tasks/13/solution.h
@@ -6,20 +6,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int MAX_ITERATIONS = 100;
-const long long K = 277;
-const long double A = 0.0;
+const int32_t MAX_ITERATIONS = 100;
+const int64_t K = 277;
+const double A = 0.0;
 const long double B = 0.5;
 const int N = 10;
-typedef long long ll;
-typedef long double ld;
+typedef long double Ld;
 
-ld Tylor_counting(ld x, int* counter_of_iterations) {
-    ld tylor_result = 0.0;
-    ld power = x;
+Ld TylorCounting(Ld x, int* counter_of_iterations) {
+    Ld tylor_result = 0.0;
+    Ld power = x;
     int n = 1;
-    ll factorial = 1;
-    ld one_tylor_member = 0;
+    int64_t factorial = 1;
+    Ld one_tylor_member = 0;
     for (int i = 0; i < MAX_ITERATIONS; ++i) {
         *counter_of_iterations += 1;
         one_tylor_member = (power / factorial);
@@ -27,7 +26,7 @@ ld Tylor_counting(ld x, int* counter_of_iterations) {
             one_tylor_member *= -1;
         }
         tylor_result += one_tylor_member;
-        if (fabsl((ld)sin(x) - tylor_result) < K * LDBL_EPSILON) {
+        if (fabsl((Ld)sin(x) - tylor_result) < K * LDBL_EPSILON) {
             return tylor_result;
         }
         power *= (x * x);
@@ -37,11 +36,11 @@ ld Tylor_counting(ld x, int* counter_of_iterations) {
     return 1.0;
 }
 
-void Output(ld tylor, double sinus, int counter, double x) {
-    printf("|  %.2f  | %.20Lf | %.20f |     %d     |\n", x, tylor, sinus, counter);
+void Output(Ld tylor, Ld sinus, int counter, Ld x) {
+    printf("|  %.2Lf  | %.20Lf | %.20f |     %d     |\n", x, tylor, sinus, counter);
 }
 
-void Print_dashs(int n) {
+void PrintDashs(int n) {
     for (int i = 0; i < n; ++i) {
         printf("~");
     }
@@ -49,14 +48,14 @@ void Print_dashs(int n) {
 }
 
 int Task() {
-    ld step = (B - A) / N;
-    ld tylor_result = 0.0;
+    Ld step = (B - A) / N;
+    Ld tylor_result = 0.0;
     int counter_of_iterations = 0;
     printf("| Number |          Tylor         |          Real          | Iteration |\n");
-    Print_dashs(72);
-    for (ld x = A; x <= B + step; x += step) {
+    PrintDashs(72);
+    for (Ld x = A; x <= B + step; x += step) {
         counter_of_iterations = 0;
-        tylor_result = Tylor_counting(x, &counter_of_iterations);
+        tylor_result = TylorCounting(x, &counter_of_iterations);
         double sinus = sin(x);
         Output(tylor_result, sin(x), counter_of_iterations, x);
     }
