d0239e8f79f082deacdaf33e1972e77d2d05544d
mashulyag <ya.merri2005@yandex.ru>
2023-12-15 14:58:56 +0300

    lalala

--- tasks/finite_state_machine/tasks/20/FSM.h
+++ tasks/finite_state_machine/tasks/20/FSM.h
@@ -1,14 +1,30 @@
 #pragma once
 
-#include <stdio.h>
-
-typedef enum states {
-    S0,
-    S1
-    // struct with states for your KA
-} State;
+#include "fsm_realization.h"
 
 int Task() {
-    // write your solution here
+    /*
+    Regular for search miles: (-?\d*)mi
+    Another for floating numbers: (-?\d+\.?\d*)mi
+
+    TESTS:
+    I have an idea to go for a 20mi ride. -> 20
+
+    */
+    FILE* file = fopen("../tasks/finite_state_machine/tasks/20/input_file.txt", "r");
+    StateMachine* sm = MakeStateMachine();
+    printf("Finite State Machine has started its execution:\n\n");
+    
+    int result = 0;
+    while (true) {
+        result = Step(sm, (char)fgetc(file));
+        if (result) {
+            printf("\nFSM has completed its execution\n");
+            break;
+        }
+    }
+
+    DeleteStateMachine(sm);
+    fclose(file);
     return 0;
 }
---
+++ tasks/finite_state_machine/tasks/20/shared_header.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {
+    NUMSEARCH = 0,
+    VALUEFORMING,
+    CONVERSIONTOKILOMETRS,
+    STATECOUNT,
+    END
+} StateName;
+
+typedef struct State State;
+
+typedef struct {
+    State* pipeline;
+    StateName cur_state;
+    char ch;
+    int num;
+} StateMachine;
+
+typedef struct State {
+    StateName name;
+    StateName (*action)(StateMachine*);
+} State;
