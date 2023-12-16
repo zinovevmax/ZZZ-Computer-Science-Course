2446775e025d4f9790e8531579c23b968a12cf8a
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2023-12-16 23:21:34 +0300

    Update solution.h

--- tasks/numerical_methods/tasks/18/solution.h
+++ tasks/numerical_methods/tasks/18/solution.h
@@ -1,19 +1,20 @@
 #pragma once
 
 #include <float.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 
-float Func1(float x) {
+double Func1(double x) {
     return x + sqrt(x) + pow(x, 1.0 / 3.0) - 2.5;
 }
 
-float Func2(float x) {
+double Func2(double x) {
     return x - 1.0 / (3.0 + sin(3.6 * x));
 }
 
-float DichotomyMethod(float lower_bound, float upper_bound, float tolerance) {
-    float root = 0.5 * (lower_bound + upper_bound);
+double DichotomyMethod(double lower_bound, double upper_bound, double tolerance) {
+    double root = 0.5 * (lower_bound + upper_bound);
     int max_iterations = 100000;
     for (int i = 0; i < max_iterations; i++) {
         if (Func1(root) == 0.0 || (upper_bound - lower_bound) <= tolerance) {
@@ -29,11 +30,11 @@ float DichotomyMethod(float lower_bound, float upper_bound, float tolerance) {
     return root;
 }
 
-float IterationMethod(float initial_guess, float tolerance) {
-    float root = initial_guess;
+double IterationMethod(double initial_guess, double tolerance) {
+    double root = initial_guess;
     int max_iterations = 100000;
     for (int i = 0; i < max_iterations; i++) {
-        float delta = Func2(root) / (1.0 - 1.0 / (3.0 + sin(3.6 * root)));
+        double delta = Func2(root) / (1.0 - 1.0 / (3.0 + sin(3.6 * root)));
         root = root - delta;
         if (delta < 0) {
             delta = -delta;
@@ -46,9 +47,9 @@ float IterationMethod(float initial_guess, float tolerance) {
 }
 
 int Task() {
-    float lower_bound = 0.4, upper_bound = 1.0, tolerance = 1e-6;
-    float root1 = DichotomyMethod(lower_bound, upper_bound, tolerance);
-    float root2 = IterationMethod((lower_bound + upper_bound) / 2.0, tolerance);
+    double lower_bound = 0.4, upper_bound = 1.0, tolerance = 1e-6;
+    double root1 = DichotomyMethod(lower_bound, upper_bound, tolerance);
+    double root2 = IterationMethod((lower_bound + upper_bound) / 2.0, tolerance);
     printf("%f\n", root1);
     printf("%f\n", root2);
     return 0;
