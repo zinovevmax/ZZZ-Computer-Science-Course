5916e1f60e3d9b28368d75b70a2154d2def9f0a6
rezect <ilyapr1568@yandex.ru>
2023-12-18 15:34:12 +0200

    pipikaka

---
+++ tasks/numerical_methods/tasks/26/header.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <float.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
+const double_t EPSILON = 1e-6;
+const int MAX_ITERATIONS = 100;
+const int A_NYUTON = 0;
+const int B_NYUTON = 1;
+const int A_DICHOTOMY = 1;
+const int B_DICHOTOMY = 3;
