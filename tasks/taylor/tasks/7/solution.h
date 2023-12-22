57f95374f46ac59018e1c8f950bcbc8dbbf03868
mnlyuzina <mnlyuzina@yandex.ru>
2023-12-22 19:03:59 +0300

    update

--- tasks/taylor/tasks/7/solution.h
+++ tasks/taylor/tasks/7/solution.h
@@ -13,19 +13,21 @@ const long double STEP = (B - A) / N;
 const uint64_t K = 100000;
 
 long double Func(long double x) {
+    // write your function here
     return (x * (3 - x)) / powl((1 - x), 3);
     return 0.0;
 }
 
 int Task() {
+    // write your solution here
     printf("                f(x) = x(3 - x) / (1 - x)^3\n");
     printf(" -------------------------------------------------------------\n");
     printf("  x   |     Taylor series      |      Function       | Iters |\n");
     printf(" -------------------------------------------------------------\n");
     long double x = A;
+    int iterations_count = 1;
+    long double sum_of_taylor = 0;
     while (fabsl(x - B) < LDBL_EPSILON * K || x < B) {
-        int iterations_count = 1;
-        long double sum_of_taylor = 0;
         while (iterations_count < MAX_ITERATIONS) {
             sum_of_taylor += (iterations_count * (iterations_count + 2)) * powl(x, iterations_count);
             if (fabsl(sum_of_taylor - Func(x)) >= LDBL_EPSILON * K) {
@@ -33,6 +35,8 @@ int Task() {
             } else {
                 printf(" %0.2Lf | %20.20Lf |% 20.15Lf | %5d |\n", x, sum_of_taylor, Func(x), iterations_count);
                 printf(" -------------------------------------------------------------\n");
+                iterations_count = 1;
+                sum_of_taylor = 0;
                 break;
             }
         }
