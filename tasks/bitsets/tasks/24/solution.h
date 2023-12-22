9cfa9c942bdda9bbf290f7207cda3f7ad16aed3e
Nikita Krushinin <144558570+qwizy@users.noreply.github.com>
2023-12-23 00:14:20 +0300

    BITSETS_24_KRUSHININ_NIKITA

--- tasks/bitsets/tasks/24/solution.h
+++ tasks/bitsets/tasks/24/solution.h
@@ -1,11 +1,38 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
 typedef uint32_t Bitset;
+const Bitset VOICED_CONSONANTS = 48446026;
+const Bitset WITHOUT_LETTERS = 0;
+
+bool areAllConsonantsUnvoiced(const Bitset text) {
+    return (text & VOICED_CONSONANTS) == WITHOUT_LETTERS;
+}
 
 int Task() {
-    // write your solution here
+    Bitset word = 0;
+    char c = 0;
+    bool theRightWordExist = false;
+
+    while ((c = (char)getchar()) != EOF) {
+        if (c == ' ' || c == '\n') {
+            if (areAllConsonantsUnvoiced(word)) {
+                theRightWordExist = true;
+            }
+            word = 0;
+            continue;
+        }
+        word = word | 1u << (c - 'a');
+    }
+
+    if (theRightWordExist) {
+        printf("yes");
+    } else {
+        printf("no");
+    }
+
     return 0;
 }
