0b09ccb8e9ce365486ccb4612126af03744f8d3e
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2023-12-24 12:48:51 +0300

    Update solution.h

--- tasks/numerical_methods/tasks/18/solution.h
+++ tasks/numerical_methods/tasks/18/solution.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define ANSWER_1 0.7376
-#define ANSWER_2 0.2624
-
 #include <float.h>
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
+
+const double ANSWER_1 = 0.7376;
+const double ANSWER_2 = 0.2624;
 
 double Func1(double x) {
     return x + sqrt(x) + pow(x, 1.0 / 3.0) - 2.5;
@@ -47,10 +47,11 @@ int Task() {
     double upper_bound_dich = 1.0;
     double lower_bound_iter = 0.0;
     double upper_bound_iter = 0.85;
+    double guess = (lower_bound_iter + upper_bound_iter) / 2.0;
     double tolerance = DBL_EPSILON;
     double root1 = DichotomyMethod(lower_bound_dich, upper_bound_dich, tolerance);
-    double root2 = IterationMethod((lower_bound_iter + upper_bound_iter) / 2.0, lower_bound_iter, upper_bound_iter, tolerance);
-    printf("%f, %f\n", root1, ANSWER_1);
-    printf("%f, %f\n", root2, ANSWER_2);
+    double root2 = IterationMethod(guess, lower_bound_iter, upper_bound_iter, tolerance);
+    printf("%f %f\n", root1, ANSWER_1);
+    printf("%f %f\n", root2, ANSWER_2);
     return 0;
 }
