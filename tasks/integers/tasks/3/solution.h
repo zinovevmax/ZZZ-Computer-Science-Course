49f1e79e607dcde2af19ee1a617c915edd8a883b
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2023-12-26 21:24:09 +0300

    Update solution.h

--- tasks/integers/tasks/3/solution.h
+++ tasks/integers/tasks/3/solution.h
@@ -1,28 +1,27 @@
+#pragma once
+
 #include <stdint.h>
 #include <stdio.h>
 
-int32_t MaxSum(int32_t num) {
+int32_t MaxSumOfTriplets(int32_t num) {
     int32_t max_sum = 0;
-    int32_t current_sum = 0;
-
     int32_t first_digit = num % 10;
     num /= 10;
     int32_t second_digit = num % 10;
     num /= 10;
-
+    int32_t third_digit = num % 10;
     while (num > 0) {
-        int32_t third_digit = num % 10;
-
-        current_sum = first_digit + second_digit + third_digit;
+        int32_t current_sum = first_digit + second_digit + third_digit;
         if (current_sum > max_sum) {
             max_sum = current_sum;
         }
-
-        first_digit = second_digit;
-        second_digit = third_digit;
         num /= 10;
+        first_digit = num % 10;
+        num /= 10;
+        second_digit = num % 10;
+        num /= 10;
+        third_digit = num % 10;
     }
-
     return max_sum;
 }
 
@@ -30,9 +29,7 @@ int Task() {
     int32_t num = 0;
     printf("Enter a number: ");
     scanf("%d", &num);
-
-    int32_t max_sum = MaxSum(num);
-    printf("Maximum sum of triplets: %d\n", max_sum);
-
+    int32_t max_sum = MaxSumOfTriplets(num);
+    printf("Maximum sum of triplets: %dn", max_sum);
     return 0;
 }
