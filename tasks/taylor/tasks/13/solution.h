dad0b5f0869079c8152ed984138269da64467ed7
rezect <ilyapr1568@yandex.ru>
2023-12-31 13:24:53 +0200

    Try

--- tasks/taylor/tasks/13/solution.h
+++ tasks/taylor/tasks/13/solution.h
@@ -11,22 +11,19 @@ const int64_t K = 277;
 const double A = 0.0;
 const long double B = 0.5;
 const int N = 10;
-typedef long double Ld;
+const int NUMBER_OF_DASHES = 72;
 
-Ld TylorCounting(Ld x, int* counter_of_iterations) {
-    Ld tylor_result = 0.0;
-    Ld power = x;
+long double TylorCounting(long double x, int* counter_of_iterations) {
+    long double tylor_result = 0.0;
+    long double power = x;
     int n = 1;
     int64_t factorial = 1;
-    Ld one_tylor_member = 0;
+    long double tylor_member = 0;
     for (int i = 0; i < MAX_ITERATIONS; ++i) {
         *counter_of_iterations += 1;
-        one_tylor_member = (power / factorial);
-        if (i % 2) {
-            one_tylor_member *= -1;
-        }
-        tylor_result += one_tylor_member;
-        if (fabsl((Ld)sin(x) - tylor_result) < K * LDBL_EPSILON) {
+        tylor_member = (power / factorial) * pow(-1, i);
+        tylor_result += tylor_member;
+        if (fabsl((long double)sin(x) - tylor_result) < K * LDBL_EPSILON) {
             return tylor_result;
         }
         power *= (x * x);
@@ -36,7 +33,7 @@ Ld TylorCounting(Ld x, int* counter_of_iterations) {
     return 1.0;
 }
 
-void Output(Ld tylor, Ld sinus, int counter, Ld x) {
+void Output(long double tylor, long double sinus, int counter, long double x) {
     printf("|  %.2Lf  | %.20Lf | %.20Lf |     %d     |\n", x, tylor, sinus, counter);
 }
 
@@ -48,12 +45,12 @@ void PrintDashs(int n) {
 }
 
 int Task() {
-    Ld step = (B - A) / N;
-    Ld tylor_result = 0.0;
+    long double step = (B - A) / N;
+    long double tylor_result = 0.0;
     int counter_of_iterations = 0;
     printf("| Number |          Tylor         |          Real          | Iteration |\n");
-    PrintDashs(72);
-    for (Ld x = A; x <= B + step; x += step) {
+    PrintDashs(NUMBER_OF_DASHES);
+    for (long double x = A; x <= B + step; x += step) {
         counter_of_iterations = 0;
         tylor_result = TylorCounting(x, &counter_of_iterations);
         Output(tylor_result, sin(x), counter_of_iterations, x);
