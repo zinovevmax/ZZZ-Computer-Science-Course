f8884c6acd00fc34e7f6a5ac28649f789eaec8d5
German2104 <139492070+German2104@users.noreply.github.com>
2024-01-02 13:10:25 +0300

    Update solution.h

--- tasks/integers/tasks/23/solution.h
+++ tasks/integers/tasks/23/solution.h
@@ -3,45 +3,41 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const uint8_t MAGIC_10 = 10;
-const uint8_t MAGIC_1 = 1;
-const uint8_t MAGIC_2 = 2;
-const uint8_t MAGIC_0 = 0;
 
-uint8_t CountRazr(uint64_t number) {
-    uint8_t count = MAGIC_0;
+uint8_t CountNumbers(uint64_t number) {
+    uint8_t count = 0;
     while (number > 0) {
         ++count;
-        number /= MAGIC_10;
+        number /= 10;
     }
     return count;
 }
 
 int64_t Pow(uint8_t first, uint8_t second) {
-    int temp = MAGIC_1;
-    if (second == MAGIC_0) {
+    int temp = 1;
+    if (second == 0) {
         return 1;
     }
-    while (second > MAGIC_0) {
+    while (second > 0) {
         temp *= first;
         --second;
     }
     return temp;
 }
 
-int Task() {
+int main() {
     uint64_t number = 0;
     scanf("%lu", &number);
-    uint8_t rz = number % MAGIC_10;
-    uint8_t temp = CountRazr(number);
+    uint8_t rz = number % 10;
+    uint8_t temp = CountNumbers(number);
     uint8_t temp_copy = temp;
-    while (temp > MAGIC_1) {
-        uint8_t rezult = number / Pow(MAGIC_10, temp - MAGIC_1);
+    while (temp > 1) {
+        uint8_t rezult = number / Pow(10, temp - 1);
         printf("%u", rezult);
-        number %= Pow(MAGIC_10, temp - MAGIC_2);
-        temp -= MAGIC_2;
+        number %= Pow(10, temp - 2);
+        temp -= 2;
     }
-    if (temp_copy % MAGIC_2 != 0) {
+    if (temp_copy % 2 != 0) {
         printf("%u", rz);
     }
     return 0;
