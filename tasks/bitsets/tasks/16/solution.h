97bd6e317e2dc72b5886e9fe83fdbba4e31e66f3
German2104 <139492070+German2104@users.noreply.github.com>
2024-01-03 12:14:00 +0300

    Update solution.h

--- tasks/bitsets/tasks/16/solution.h
+++ tasks/bitsets/tasks/16/solution.h
@@ -1,27 +1,22 @@
 #pragma once
 
-#include <ctype.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
-unsigned int CharToSet(int c) {
-    c = tolower(c);
-    if (c < 'a' || c > 'z' || c == 'a' || c == 'u' || c == 'o' || c == 'e' || c == 'i') {
-        return 0;
-    } else {
-        return 1u << (c - 'a');
-    }
-}
+typedef uint32_t Bitset;
+
+const Bitset VOWELS = 17842449;
 
 int Task() {
-    int c = 0;
-    unsigned int letters_set_1 = 0;
-    unsigned int letters_set_2 = 0;
+    char c = 0;
+    Bitset letters_set_1 = 0;
+    Bitset letters_set_2 = 0;
     bool answer = false;
-    while ((c = getchar()) != EOF) {
-        letters_set_2 = letters_set_2 | CharToSet(c);
+    while ((c =(char) getchar()) != EOF) {
+        letters_set_2 = letters_set_2 | 1u << (c - 'a');
         if (c == ' ' || c == '\n') {
-            if (letters_set_2 == letters_set_1) {
+            if ((letters_set_2 & VOWELS) == (letters_set_1 & VOWELS)) {
                 answer = true;
             }
             letters_set_1 = letters_set_2;
