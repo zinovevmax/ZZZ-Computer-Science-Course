7a938ef165f3e44f8311e2bf9cd77532bcf26de4
GorbanArtemiy <artygor5@yandex.ru>
2024-01-04 16:55:29 +0300

    работает 100%

--- tasks/integers/tasks/17/solution.h
+++ tasks/integers/tasks/17/solution.h
@@ -15,7 +15,7 @@ int Task() {
     length_counter = length_counter / 10;
     uint32_t last_digit = number % 10;
     uint32_t new_number = last_digit * length_counter + ((number % length_counter) / 10) * 10 + first_digit;
-    if (length_counter == 1){
+    if (length_counter == 1) {
         new_number = new_number / 2;
     }
     printf("%d", new_number);
