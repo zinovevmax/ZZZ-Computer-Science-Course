34d54bbadc5edd16b8a830e339f7809d7790d34a
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-01-04 20:08:30 +0300

    Update solution.h

--- tasks/matrix/tasks/10/solution.h
+++ tasks/matrix/tasks/10/solution.h
@@ -35,8 +35,8 @@ void ShiftColumnsGroup(Matrix matrix, int32_t n, int32_t group, int32_t shift) {
     }
 }
 
-void ShiftAllColumnsGroups(Matrix matrix, int32_t n, int32_t groupSize, int32_t shift) {
-    for (int32_t group = 0; group < n; group += groupSize) {
+void ShiftAllColumnsGroups(Matrix matrix, int32_t n, int32_t group_size, int32_t shift) {
+    for (int32_t group = 0; group < n; group += group_size) {
         ShiftColumnsGroup(matrix, n, group, shift);
     }
 }
@@ -51,37 +51,37 @@ void PrintMatrix(Matrix matrix, int32_t n) {
 }
 
 Matrix InitializeMatrixAndShift(const char* filename, int32_t* n) {
-    int32_t matrixShift = 3;  // можно поменять на подходящее значение
-    if (matrixShift < 0) {
+    int32_t matrix_shift = 3;  // можно поменять на подходящее значение
+    if (matrix_shift < 0) {
         printf("Invalid input\n");
         abort();
     }
     FILE* file = fopen(filename, "r");
     fscanf(file, "%d", n);
-    int32_t matrixSize = (*n) * (*n);
-    if (matrixShift > matrixSize) {
-        matrixShift = matrixShift % matrixSize;
+    int32_t matrix_size = (*n) * (*n);
+    if (matrix_shift > matrix_size) {
+        matrix_shift = matrix_shift % matrix_size;
     }
     Matrix matrix = CreateMatrix(*n);
-    int32_t* startElements = (int32_t*)malloc((matrixSize - matrixShift) * sizeof(int32_t));
-    int32_t readElementsMatrixCounter = matrixSize;
-    int32_t newElementsCount = 0;
-    for (int i = 0; i < matrixSize - matrixShift; ++i) {
-        fscanf(file, "%d", &startElements[i]);
+    int32_t* start_elements = (int32_t*)malloc((matrix_size - matrix_shift) * sizeof(int32_t));
+    int32_t read_elements_matrix_counter= matrix_size;
+    int32_t new_elements_count = 0;
+    for (int i = 0; i < matrix_size - matrix_shift; ++i) {
+        fscanf(file, "%d", &start_elements[i]);
     }
     for (int i = 0; i < *n; ++i) {
         for (int j = 0; j < *n; ++j) {
-            if (readElementsMatrixCounter > matrixSize - matrixShift) {
+            if (read_elements_matrix_counter > matrix_size - matrix_shift) {
                 fscanf(file, "%d", &matrix[i][j]);
-            } else if (readElementsMatrixCounter <= matrixSize - matrixShift) {
-                matrix[i][j] = startElements[newElementsCount];
-                ++newElementsCount;
+            } else if (read_elements_matrix_counter <= matrix_size - matrix_shift) {
+                matrix[i][j] = start_elements[new_elements_count];
+                ++new_elements_count;
             }
             --readElementsMatrixCounter;
         }
     }
     fclose(file);
-    free(startElements);
+    free(start_elements);
     return matrix;
 }
 
@@ -89,9 +89,9 @@ int Task() {
     int32_t n = 0;
     const char* filename = "../tasks/matrix/tasks/10/matrix.txt";
     Matrix matrix = InitializeMatrixAndShift(filename, &n);
-    int32_t groupSize = 1;  // можно поменять на подходящее значение
+    int32_t group_size = 1;  // можно поменять на подходящее значение
     int32_t shift = 3;      // можно поменять на подходящее значение
-    ShiftAllColumnsGroups(matrix, n, groupSize, shift);
+    ShiftAllColumnsGroups(matrix, n, group_size, shift);
     PrintMatrix(matrix, n);
     MatrixFree(matrix, n);
     return 0;
