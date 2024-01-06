ebe53c0436f39952344aaec30f8f66ae05215101
zinovevmax <zinomax2005@gmail.com>
2024-01-06 14:20:05 +0300

    lol

--- tasks/bitsets/tasks/11/solution.h
+++ tasks/bitsets/tasks/11/solution.h
@@ -1,11 +1,37 @@
-#pragma once
-
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef uint32_t Bitset;
+const Bitset CONSONANTS = 17842449;  // 01000100000100000100010001
+const int LETTRRS_IN_ALFABET = 26;
+
+uint32_t CheckOnlyVovel(Bitset word) {
+    return (word & CONSONANTS);
+}
+
+int Answer(int a) {
+    if (a > 0) {
+        printf("there are vowels included in all words\n");
+    } else {
+        printf("there are no vowels included in all words\n");
+    }
+    return 0;
+}
 
 int Task() {
-    // write your solution here
+    Bitset word = 0;
+    Bitset vowel_in_word = 0;
+    Bitset count = 17842449;
+    char words[] = "wrte gtfe nhte vcxe";  // write words
+    for (size_t i = 0; i <= strlen(words); ++i) {
+        if (words[i] >= 'a' && words[i] <= 'z') {
+            word |= (1u << (words[i] - 'a'));
+        } else {
+            count &= CheckOnlyVovel(word);
+            word = 0;
+        }
+    }
+    Answer(count);
     return 0;
 }
